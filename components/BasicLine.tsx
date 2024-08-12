e75f69a01a28ad335c38211ca61d16179c516db5
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-08-11 18:30:23 -0700

    design: add charts with BasicLine component

---
+++ components/BasicLine.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { Line, LineChart } from "recharts"
+
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart"
+import { cn } from "@/lib/utils"
+import { HTMLAttributes } from "react"
+
+const defaultChartConfig = {
+  value: {
+    color: "hsla(var(--chart-1))",
+  },
+} satisfies ChartConfig
+
+type BasicLineProps = HTMLAttributes<HTMLDivElement> & {
+  chartData: { value: number }[]
+  chartConfig?: ChartConfig
+}
+
+const BasicLine = ({ className, chartData, chartConfig = defaultChartConfig }: BasicLineProps) => (
+  <ChartContainer config={chartConfig} className={cn("min-h-[100px] w-full", className)}>
+    <LineChart accessibilityLayer data={chartData}>
+      <Line dataKey="value" stroke="var(--color-value)" radius={4} dot={false} strokeWidth={2} type="natural" />
+      <ChartTooltip content={<ChartTooltipContent />} />
+      </LineChart>
+  </ChartContainer>
+)
+
+export default BasicLine
