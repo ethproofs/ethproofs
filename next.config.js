8e5008afa58514a77328c187753ec0ba79cebb3e
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-10-18 15:19:07 -0700

    chore(deps): pnpm i -D @svgr/webpack
    webpack svgr setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    )
+    fileLoaderRule.exclude = /\.svg$/
+
+    // Convert all other *.svg imports to React components
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: fileLoaderRule.issuer,
+      resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+      use: ["@svgr/webpack"],
+    })
+
+    return config
+  },
+}
 
 module.exports = nextConfig
