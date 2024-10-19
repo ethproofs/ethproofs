f287d9c734cd88dca17b213429745171b4d9ebbd
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-10-18 18:45:05 -0700

    feat: implement isSecondary for ui/button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,7 +11,9 @@ const buttonVariants = cva(
     // Base default styling is "outline" pattern, primary color for text, border matches, no bg
     "text-primary border-current",
     // Hover: Default hover adds box-shadow, text (border) to --primary-hover
-    "hover:bg-primary-border"
+    "hover:bg-primary-border",
+    // isSecondary: Switch text (border) to --body instead of --primary
+    "[&[data-secondary='true']]:text-body"
   ),
   {
     variants: {
@@ -38,19 +40,56 @@ const buttonVariants = cva(
   }
 )
 
+type ButtonVariantProps = VariantProps<typeof buttonVariants>
+
+export const checkIsSecondary = ({
+  variant,
+  isSecondary,
+}: {
+  variant: ButtonVariantProps["variant"]
+  isSecondary: boolean
+}) => {
+  // These two variants do not have secondary styling, so prevent overrides
+  return {
+    "data-secondary":
+      !["solid", "link"].includes(variant || "solid") && isSecondary,
+  }
+}
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  /**
+   * Custom theme prop. If true, `body` color is used instead of
+   * `primary` color in the theming.
+   *
+   * `NOTE`: Does not apply to the `Solid` or `Link` variants
+   */
+  isSecondary?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      size,
+      isSecondary = false,
+      asChild = false,
+      ...props
+    },
+    ref
+  ) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        {...checkIsSecondary({
+          variant,
+          isSecondary,
+        })}
         {...props}
       />
     )
