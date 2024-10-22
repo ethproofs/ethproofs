8c620b31637f51939d6ae1a27f304fc3577c7bdd
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-10-21 20:25:23 -0700

    feat: pnpm dlx shadcn@latest add input

---
+++ components/ui/input.tsx
@@ -0,0 +1,28 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-fit w-full gap-2 rounded-full border border-primary bg-background-highlight px-4 py-3 text-xs ring-offset-background [&>svg]:text-primary",
+          "placeholder:text-body-secondary disabled:cursor-not-allowed disabled:opacity-50",
+          "focus-visible:ring-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
+          "file:text-foreground file:border-0 file:bg-transparent file:text-sm file:font-medium",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Input.displayName = "Input"
+
+export { Input }
