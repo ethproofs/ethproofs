e66d03043470fa671d46de6d94bf6415ddcc0bd0
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-10-21 20:26:26 -0700

    chore: trim landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
-import Link from "next/link"
-
 export default async function Index() {
   return (
     <div className="flex w-full flex-1 flex-col items-center gap-20">
-      <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
-        <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm">
-          <h1>ETH Proofs</h1>
-
-          <Link
-            href="/proofs"
-            className="bg-btn-background hover:bg-btn-background-hover flex rounded-md px-3 py-2 no-underline"
-          >
-            Proofs
-          </Link>
-        </div>
-      </nav>
+      <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm">
+        <h1>ETH Proofs</h1>
+      </div>
     </div>
   )
 }
