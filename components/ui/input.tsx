37d60e9dc17bf931e7470f040014aef1884415a6
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-10-22 19:38:50 -0700

    fix: shrink-0 input svgs

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-fit w-full gap-2 rounded-full border border-primary bg-background-highlight px-4 py-3 text-xs ring-offset-background [&>svg]:text-primary",
+          "flex h-fit w-full gap-2 rounded-full border border-primary bg-background-highlight px-4 py-3 text-xs ring-offset-background [&>svg]:text-primary [&>svg]:shrink-0",
           "placeholder:text-body-secondary disabled:cursor-not-allowed disabled:opacity-50",
           "focus-visible:outline-none",
           "file:text-foreground file:border-0 file:bg-transparent file:text-sm file:font-medium",
