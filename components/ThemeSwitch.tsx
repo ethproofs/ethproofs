cdcc6cbda43023da6547732e8d5f36a7e456475b
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-10-23 16:51:20 -0700

    fix: ThemeSwitch menu component

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -6,14 +6,8 @@ import { useTheme } from "next-themes"
 import Moon from "@/components/svgs/moon.svg"
 import Sun from "@/components/svgs/sun.svg"
 import System from "@/components/svgs/monitor-cog.svg"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "./ui/select"
-import ThemeIcon from "./ui/ThemeIcon"
+import { Select, SelectContent, SelectItem, SelectTrigger } from "./ui/select"
+import ThemeIcon from "./ui/theme-icon"
 
 const ThemeSwitch = () => {
   const [mounted, setMounted] = useState(false)
@@ -29,22 +23,22 @@ const ThemeSwitch = () => {
   return (
     <Select value={theme} onValueChange={setTheme}>
       <SelectTrigger className="box-content h-3.5 w-fit items-center gap-2 px-2 py-1.5">
-        <SelectValue placeholder={<ThemeIcon />} />
+        <ThemeIcon />
       </SelectTrigger>
       <SelectContent>
         <SelectItem value="light">
           <div className="flex items-center gap-2">
-            <Sun /> <span className="sr-only">Light</span>
+            <Sun /> Light
           </div>
         </SelectItem>
         <SelectItem value="dark">
           <div className="flex items-center gap-2">
-            <Moon /> <span className="sr-only">Dark</span>
+            <Moon /> Dark
           </div>
         </SelectItem>
         <SelectItem value="system">
           <div className="flex items-center gap-2">
-            <System /> <span className="sr-only">System</span>
+            <System /> System
           </div>
         </SelectItem>
       </SelectContent>
