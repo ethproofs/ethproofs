9cc416716480bddbdefec5279266d73fd9ea93e2
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-10-29 18:04:57 +0700

    feat: match ThemeSwitch  to DS, use dropdown menu

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -3,12 +3,19 @@
 import { useState, useEffect } from "react"
 import { useTheme } from "next-themes"
 
+import ArrowDropdown from "@/components/svgs/arrow-dropdown.svg"
 import Moon from "@/components/svgs/moon.svg"
 import Sun from "@/components/svgs/sun.svg"
 import System from "@/components/svgs/monitor.svg"
-import { Select, SelectContent, SelectItem, SelectTrigger } from "./ui/select"
 import ThemeIcon from "./ui/theme-icon"
 import useThemingKeyboardShortcuts from "@/hooks/useThemingKeyboardShortcuts"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu"
+import { cn } from "@/lib/utils"
 
 const ThemeSwitch = () => {
   const [mounted, setMounted] = useState(false)
@@ -27,29 +34,49 @@ const ThemeSwitch = () => {
 
   if (!mounted) return null
 
+  const items = [
+    {
+      id: "light",
+      label: "Light",
+      icon: Sun,
+    },
+    {
+      id: "dark",
+      label: "Dark",
+      icon: Moon,
+    },
+    {
+      id: "system",
+      label: "System",
+      icon: System,
+    },
+  ]
   return (
-    <Select value={theme} onValueChange={setTheme}>
-      <SelectTrigger className="box-content h-3.5 w-fit items-center gap-2 px-2 py-1.5">
-        <ThemeIcon className="!text-body" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectItem value="light">
-          <div className="flex items-center gap-2">
-            <Sun /> Light
-          </div>
-        </SelectItem>
-        <SelectItem value="dark">
-          <div className="flex items-center gap-2">
-            <Moon /> Dark
-          </div>
-        </SelectItem>
-        <SelectItem value="system">
-          <div className="flex items-center gap-2">
-            <System /> System
-          </div>
-        </SelectItem>
-      </SelectContent>
-    </Select>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div className="box-content flex h-3.5 w-fit items-center gap-2 rounded-full border border-primary px-2 py-1.5 text-primary">
+          <ThemeIcon className="!text-body" />
+          <ArrowDropdown />
+        </div>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent>
+        {items.map((item) => (
+          <DropdownMenuItem
+            key={item.id}
+            onClick={() => setTheme(item.id)}
+            className={cn(
+              theme === item.id && "bg-green-300 dark:bg-primary-border"
+            )}
+          >
+            <div className="flex items-center gap-2 p-2.5">
+              <item.icon />
+              {item.label}
+            </div>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
