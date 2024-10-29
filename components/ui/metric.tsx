88399ff2c6415f8de6e511aff4eddb744a8fba17
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-10-29 23:02:25 +0700

    feat: create ui/metric reusable components

---
+++ components/ui/metric.tsx
@@ -0,0 +1,71 @@
+import React from "react"
+
+import InfoCircle from "@/components/svgs/info-circle.svg"
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./tooltip"
+import { cn } from "@/lib/utils"
+
+const MetricBox = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("space-y-0.5 px-2 py-3", className)}
+    {...props}
+  />
+))
+MetricBox.displayName = "MetricBox"
+
+const MetricLabel = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "flex items-center gap-2 text-sm text-body-secondary",
+      className
+    )}
+    {...props}
+  />
+))
+MetricLabel.displayName = "MetricLabel"
+
+const MetricInfo = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, children, ...props }, ref) => (
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger>
+        <InfoCircle />
+      </TooltipTrigger>
+      <TooltipContent>
+        <p ref={ref} {...props}>
+          {children}
+        </p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+))
+MetricInfo.displayName = "MetricInfo"
+
+const MetricValue = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("font-mono text-2xl font-semibold", className)}
+    {...props}
+  />
+))
+MetricValue.displayName = "MetricValue"
+
+export { MetricBox, MetricLabel, MetricInfo, MetricValue }
