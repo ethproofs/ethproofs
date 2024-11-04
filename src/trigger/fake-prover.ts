6b2b37b940ab80b7cce752d454cd01001ed51d57
Pablo Pettinari <pettinarip@gmail.com>
2024-11-04 16:44:38 +0100

    fake prover task

---
+++ src/trigger/fake-prover.ts
@@ -0,0 +1,69 @@
+import { createPublicClient, http } from "viem"
+import { mainnet } from "viem/chains"
+import { faker } from "@snaplet/copycat"
+import { logger, schedules } from "@trigger.dev/sdk/v3"
+
+const client = createPublicClient({
+  chain: mainnet,
+  // TODO use custom rpc endpoint
+  transport: http(),
+})
+
+const SITE_URL = process.env.SITE_URL || "http://localhost:3000"
+const API_KEYS = process.env.API_KEYS?.split(",") || []
+
+const generateFakeProof = (blockNumber: bigint) => {
+  return {
+    block_number: Number(blockNumber),
+    proof: "binary_proof_data",
+    proof_status: faker.helpers.arrayElement(["proved", "proving", "queued"]),
+    prover_machine: faker.number.int({ min: 1, max: 10 }),
+    prover_duration: faker.number.int({ min: 1, max: 1000 }),
+    proving_cost: faker.number.float({ min: 0.01, max: 0.1 }),
+    proving_cycles: faker.number.int({ min: 1000000, max: 10000000 }),
+  }
+}
+
+export const fakeProverTask = schedules.task({
+  id: "fake-prover-task",
+  //every 20 minutes
+  cron: "*/20 * * * *",
+  run: async () => {
+    // fetch last block using viem
+    const block = await client.getBlock({ blockTag: "safe" })
+    const blockNumber = block.number
+
+    logger.info(`Generating proofs for block ${blockNumber}`, {
+      url: `${SITE_URL}/api/proofs`,
+    })
+
+    for (const apiKey of API_KEYS) {
+      const proof = generateFakeProof(blockNumber)
+      logger.info(
+        `Submitting proof for block ${blockNumber} with API key ${apiKey.slice(0, 8)}...`,
+        { proof }
+      )
+
+      try {
+        const response = await fetch(`${SITE_URL}/api/proofs`, {
+          method: "POST",
+          body: JSON.stringify(proof),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${apiKey}`,
+          },
+        })
+
+        if (!response.ok) {
+          logger.error(
+            `Failed to submit proof: ${response.status} ${response.statusText}, ${await response.text()}`
+          )
+        } else {
+          logger.info(`Successfully submitted proof for block ${blockNumber}`)
+        }
+      } catch (error) {
+        logger.error(`Error submitting proof: ${error}`)
+      }
+    }
+  },
+})
