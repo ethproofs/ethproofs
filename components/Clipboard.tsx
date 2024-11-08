24d181402313704846b3bf350c7b24ef434c1c7e
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-11-08 12:05:33 +0700

    chore: remove unused imports

--- components/Clipboard.tsx
+++ components/Clipboard.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useState, useEffect, useRef } from "react"
 import { cn } from "@/lib/utils"
 import { useClipboard } from "@/hooks/useClipboard"
 import { Button } from "@/components/ui/button"
