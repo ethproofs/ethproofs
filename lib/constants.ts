978fbd2b9f5974dc245c6bdddb938cb854948809
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-11-11 16:22:27 +0700

    feat: add metadataBase with env SITE_URL

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const SITE_NAME = "EthProofs"
 export const SITE_DESCRIPTION = "Building a fully SNARKed Ethereum"
-export const SITE_URL = "https://ethproofs.org"
+export const SITE_URL = process.env.SITE_URL || "https://ethproofs.org"
 export const SITE_PREVIEW_URL = "http://ethproofs.netlify.app"
 export const SITE_REPO_URL = "https://github.com/ethproofs/ethproofs"
 
