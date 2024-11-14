1dd6d380f685715bf57d53e110285e12a41670b3
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-11-14 18:55:38 +0700

    init: basic search queries

--- components/header/SearchInput.tsx
+++ components/header/SearchInput.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import Magnifier from "@/components/svgs/magnifier.svg"
 import { Input } from "@/components/ui/input"
@@ -9,12 +9,50 @@ import { cn } from "@/lib/utils"
 
 import { useContainerQuery } from "@/hooks/useContainerQuery"
 import useSearchKeyboardShortcuts from "@/hooks/useSearchKeyboardShortcuts"
+import { createClient } from "@/utils/supabase/client"
 
 const PLACEHOLDER = "Search by slot number / hash / number / prover block"
 const k = 6.5
+const supabase = createClient()
 
 const SearchInput = ({ className }: React.HTMLAttributes<HTMLInputElement>) => {
   const [query, setQuery] = useState("")
+  const [match, setMatch] = useState<string | null>(null)
+
+  useEffect(() => {
+    const handler = setTimeout(async () => {
+      if (!query) {
+        setMatch(null)
+        return
+      }
+
+      const { data: blockMatch } = await supabase
+        .from("blocks")
+        .select("*,block_number::text")
+        .eq("block_number", query)
+        .single()
+      if (blockMatch?.block_number) {
+        setMatch(blockMatch?.block_number)
+        return
+      }
+
+      const { data: hashMatch } = await supabase
+        .from("blocks")
+        .select("*,hash::text")
+        .eq("hash", query)
+        .single()
+      if (hashMatch?.hash) {
+        setMatch(hashMatch?.hash)
+        return
+      }
+
+      setMatch("No results found")
+    }, 750) // 750 ms debounce
+
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [query])
 
   const { inputRef } = useSearchKeyboardShortcuts()
 
@@ -39,6 +77,11 @@ const SearchInput = ({ className }: React.HTMLAttributes<HTMLInputElement>) => {
       >
         <Magnifier className="text-primary" />
       </div>
+      {match && (
+        <div className="absolute inset-y-0 start-0 top-[150%] flex items-center ps-4 lg:m-0">
+          <span className="text-sm text-primary">{match}</span>
+        </div>
+      )}
     </div>
   )
 }
