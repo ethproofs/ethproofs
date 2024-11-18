07820de1538bb97edebac247bea882be4de1db9c
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-11-18 18:21:35 +0700

    feat: coming soon page only

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,7 @@
-/* eslint-disable simple-import-sort/imports */
-
 import { ThemeProvider } from "next-themes"
-import Link from "next/link"
 
-import ActiveLinkDecorator from "@/components/ActiveLinkDecorator"
-import HeaderScrollEffects from "@/components/header/HeaderScrollEffects"
 import ThemeSwitch from "@/components/header/ThemeSwitch"
-import EthProofsLogo from "@/components/svgs/eth-proofs-logo.svg"
-import GitHub from "@/components/svgs/github.svg"
-import Hamburger from "@/components/svgs/hamburger.svg"
-import Magnifier from "@/components/svgs/magnifier.svg"
-import { Button } from "@/components/ui/button"
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer"
 
-import { cn } from "@/lib/utils"
-
-import { SITE_REPO_URL } from "@/lib/constants"
-
-import SearchInput from "@/components/header/SearchInput"
 import "../styles/globals.css"
 
 export default function RootLayout({
@@ -48,129 +23,10 @@ export default function RootLayout({
       <body className="pb-80">
         <ThemeProvider attribute="class">
           <div className="mx-auto flex max-w-screen-2xl flex-col gap-16 px-4 sm:px-4 md:px-8 xl:px-16">
-            <header
-              className={cn(
-                "sticky top-0 z-10 grid grid-cols-3 items-center py-4",
-                "-mx-4 sm:-mx-4 md:-mx-8 xl:-mx-16",
-                "px-4 sm:px-4 md:px-8 xl:px-16"
-              )}
-            >
-              <HeaderScrollEffects />
-
-              {/* Header start */}
-              <div className="flex">
-                <Link href="/">
-                  <EthProofsLogo />
-                </Link>
-              </div>
-
-              {/* Header center */}
-              <div className="flex justify-center">
-                <SearchInput className="max-md:hidden" />
-              </div>
-
-              {/* Header end */}
-              <div className="flex justify-end">
-                <nav className="flex gap-6 max-md:hidden">
-                  <Link
-                    href="/"
-                    className="relative h-fit outline-offset-4 hover:text-primary-light"
-                  >
-                    Proofs
-                    <ActiveLinkDecorator match="/" />
-                  </Link>
-                  <Link
-                    href="/about"
-                    className="relative h-fit outline-offset-4 hover:text-primary-light"
-                  >
-                    About
-                    <ActiveLinkDecorator match="/about" />
-                  </Link>
-                  <ThemeSwitch />
-                </nav>
-
-                {/* Mobile */}
-                <div className="flex gap-4 md:hidden">
-                  <Button variant="solid" className="size-[2rem] p-2">
-                    {/* TODO: Implement search */}
-                    <Magnifier />
-                  </Button>
-                  <Drawer>
-                    <DrawerTrigger asChild>
-                      <Button variant="solid" className="size-[2rem] p-2">
-                        <Hamburger />
-                      </Button>
-                    </DrawerTrigger>
-                    <DrawerContent>
-                      <DrawerHeader className="space-y-16">
-                        <DrawerTitle className="mb-4 flex items-center justify-between gap-4">
-                          <Link href="/" className="ms-4">
-                            <EthProofsLogo />
-                          </Link>
-                          <DrawerClose asChild>
-                            <Button variant="ghost">Close</Button>
-                          </DrawerClose>
-                        </DrawerTitle>
-                        <DrawerDescription className="space-y-12">
-                          <nav>
-                            <ul className="space-y-12 text-center">
-                              <li className="list-none">
-                                <Button variant="ghost" size="lg" asChild>
-                                  <Link href="/">Proofs</Link>
-                                </Button>
-                              </li>
-                              <li className="list-none">
-                                <Button variant="ghost" size="lg" asChild>
-                                  <Link href="/about">About</Link>
-                                </Button>
-                              </li>
-                            </ul>
-                          </nav>
-                          <div className="flex justify-center">
-                            <ThemeSwitch />
-                          </div>
-                        </DrawerDescription>
-                      </DrawerHeader>
-                      <DrawerFooter>
-                        <nav className="flex justify-center">
-                          <Button variant="ghost" size="lg" asChild>
-                            <Link href={SITE_REPO_URL}>
-                              <GitHub /> GitHub
-                            </Link>
-                          </Button>
-                        </nav>
-                      </DrawerFooter>
-                    </DrawerContent>
-                  </Drawer>
-                </div>
-              </div>
+            <header className="flex h-8 justify-end py-4">
+              <ThemeSwitch />
             </header>
-
             <main className="min-h-[50vh]">{children}</main>
-
-            <footer className="mx-auto mt-16 flex max-w-prose flex-col items-center">
-              <Button asChild size="lg">
-                <Link href={SITE_REPO_URL} className="mb-12">
-                  <GitHub className="size-6" />
-                  <span>Contribute to ETH Proofs</span>
-                </Link>
-              </Button>
-
-              <p className="text-center">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Similique facere temporibus minima quibusdam ratione.
-              </p>
-              <p className="text-center text-lg text-primary-light">
-                Public goods are good
-              </p>
-
-              <Link
-                href={new URL("/issues/new/", SITE_REPO_URL).toString()}
-                className="mt-8 text-center text-body-secondary"
-              >
-                Spot a bug? Report it here
-              </Link>
-            </footer>
           </div>
         </ThemeProvider>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,77 +1,20 @@
 import type { Metadata } from "next"
 import Image from "next/image"
 
-import type { SummaryItem } from "@/lib/types"
-
-import BlocksTable from "@/components/BlocksTable"
-import Block from "@/components/svgs/box.svg"
-import Clock from "@/components/svgs/clock.svg"
-import DollarSign from "@/components/svgs/dollar-sign.svg"
+import EthProofsLogo from "@/components/svgs/eth-proofs-logo.svg"
 
 import { cn } from "@/lib/utils"
 
 import { getMetadata } from "@/lib/metadata"
-import { formatNumber } from "@/lib/number"
 import HeroDark from "@/public/images/hero-background.png"
-import { createClient } from "@/utils/supabase/server"
 
 export const metadata: Metadata = getMetadata()
 
 export default async function Index() {
-  const supabase = createClient({
-    global: {
-      fetch: (input, init) =>
-        fetch(input, {
-          ...init,
-          cache: "force-cache",
-          next: { tags: ["blocks", "proofs"] },
-        }),
-    },
-  })
-
-  const summary = await supabase.from("recent_summary").select().single()
-
-  const blocksResponse = await supabase
-    .from("blocks")
-    .select(`*,proofs!inner(id:proof_id)`)
-    .order("block_number", { ascending: false })
-
-  const blocks = blocksResponse.data || []
-
-  const proofsResponse = await supabase.from("proofs").select()
-  const proofs = proofsResponse.data || []
-
-  const summaryItems: SummaryItem[] = summary.data
-    ? [
-        {
-          label: "Proven blocks",
-          icon: <Block />,
-          value: formatNumber(summary.data?.total_proven_blocks || 0),
-        },
-        {
-          label: "Avg cost per proof",
-          icon: <DollarSign />,
-          value: formatNumber(summary.data?.avg_cost_per_proof || 0, {
-            maximumFractionDigits: 2,
-          }),
-        },
-        {
-          label: "Avg proof latency",
-          icon: <Clock />,
-          value: formatNumber(summary.data?.avg_proof_latency || 0, {
-            style: "unit",
-            unit: "second",
-            unitDisplay: "narrow",
-            maximumFractionDigits: 0,
-          }),
-        },
-      ]
-    : []
-
   return (
     <div className="flex w-full flex-1 flex-col items-center gap-20">
       <div
-        className="absolute inset-0 -z-10 h-[28rem] md:max-xl:h-96"
+        className="absolute inset-0 -z-10 h-[26rem] md:max-xl:h-[22rem]"
         style={{ mask: "linear-gradient(180deg, white 80%, transparent)" }}
       >
         <Image
@@ -90,37 +33,18 @@ export default async function Index() {
       </div>
       <div className="mt-56 flex w-full flex-col items-center justify-between gap-4 p-3 md:mt-44 xl:mt-64">
         <h1 className="w-full text-center font-mono font-semibold">
-          Building fully SNARKed <span className="text-primary">Ethereum</span>
+          SNARKifying <span className="text-primary">Ethereum</span>
         </h1>
-        <p className="max-w-2xl text-center text-lg">
-          This is a proof of concept that ZK proves 1-of-N blocks. Eventually,
-          it will enable full ZK light clients on any smartphone.
-        </p>
-        <div className="flex w-full max-w-2xl justify-around">
-          {summaryItems.map(({ label, icon, value }) => (
-            <div key={label} className="flex flex-col gap-1 p-2">
-              <div className="flex flex-col items-center justify-center gap-x-2 md:flex-row">
-                <p className="font-mono text-2xl text-primary md:text-3xl lg:text-4xl">
-                  {icon}
-                </p>
-                <p className="font-mono text-2xl text-primary md:text-3xl lg:text-4xl">
-                  {value}
-                </p>
-              </div>
-              <div>
-                <p className="text-center text-xs uppercase md:text-sm">
-                  {label}
-                </p>
-              </div>
-              <p className="text-center text-xs font-bold uppercase text-body-secondary">
-                Last 30 days
-              </p>
-            </div>
-          ))}
+        <span className="my-2 text-xl">
+          <span className="font-mono font-bold">soon™️</span> with
+        </span>
+        <div className="max-w-40 md:max-w-60 lg:max-w-80">
+          <EthProofsLogo
+            alt="EthProofs logo"
+            className="size-full object-contain"
+          />
         </div>
       </div>
-
-      <BlocksTable blocks={blocks || []} proofs={proofs} />
     </div>
   )
 }
