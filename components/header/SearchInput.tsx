f7ee43433a28a241dbc262fcd424586c28c29548
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-11-26 13:30:13 +0700

    feat: implement basic search

--- components/header/SearchInput.tsx
+++ components/header/SearchInput.tsx
@@ -1,6 +1,12 @@
 "use client"
 
 import { useEffect, useState } from "react"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { useEventListener } from "usehooks-ts"
+import { isHash } from "viem"
+
+import { Block } from "@/lib/types"
 
 import Magnifier from "@/components/svgs/magnifier.svg"
 import { Input } from "@/components/ui/input"
@@ -10,35 +16,51 @@ import { cn } from "@/lib/utils"
 import { useContainerQuery } from "@/hooks/useContainerQuery"
 import useSearchKeyboardShortcuts from "@/hooks/useSearchKeyboardShortcuts"
 import { createClient } from "@/utils/supabase/client"
-import { Block } from "@/lib/types"
 
 const PLACEHOLDER = "Search by block number or hash"
 const k = 6.5
 const supabase = createClient()
 
 const SearchInput = ({ className }: React.HTMLAttributes<HTMLInputElement>) => {
+  const router = useRouter()
   const [query, setQuery] = useState("")
-  const [blockMatch, setBlockMatch] = useState<Block | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [blockMatch, setBlockMatch] = useState<
+    (Block & { proofs: { proof_status: string }[] }) | null
+  >(null)
+
+  useEventListener("keydown", (e) => {
+    if (e.key !== "Enter" || !blockMatch) return
+    const path = `/block/${blockMatch[isHash(query) ? "hash" : "block_number"]}`
+    setQuery("")
+    router.push(path)
+  })
 
   useEffect(() => {
+    setLoading(true)
     if (!query) {
       setBlockMatch(null)
+      setLoading(false)
       return
     }
     const handler = setTimeout(async () => {
-      const hashRegExp = /^0x[0-9a-fA-F]{64}$/
-
       const { data: match, error } = await supabase
         .from("blocks")
-        .select("*")
-        .eq(hashRegExp.test(query) ? "hash" : "block_number", query)
+        .select(
+          `*,
+          proofs!inner(proof_status)
+        `
+        )
+        .eq(isHash(query) ? "hash" : "block_number", query)
         .single()
       if (!error && match) {
         setBlockMatch(match)
+        setLoading(false)
         return
       }
 
       setBlockMatch(null)
+      setLoading(false)
     }, 250) // 250 ms debounce
 
     return () => {
@@ -53,12 +75,13 @@ const SearchInput = ({ className }: React.HTMLAttributes<HTMLInputElement>) => {
   const placeholder = isLarge ? PLACEHOLDER : PLACEHOLDER.split(" ")[0]
 
   return (
-    <div ref={containerRef} className={cn("relative w-full", className)}>
+    <div ref={containerRef} className={cn("relative z-0 w-full", className)}>
       <Input
         ref={inputRef}
         type="search"
         onChange={(e) => setQuery(e.target.value)}
         placeholder={placeholder}
+        value={query}
       />
       <div
         className={cn(
@@ -68,14 +91,40 @@ const SearchInput = ({ className }: React.HTMLAttributes<HTMLInputElement>) => {
       >
         <Magnifier className="text-primary" />
       </div>
-      {blockMatch && (
-        <div className="absolute inset-x-0 top-0 -z-10 flex h-fit flex-col rounded-b-2xl rounded-t-3xl border border-body-secondary bg-background bg-gradient-to-b from-white/[0.06] to-white/[0.12] ps-4 pt-12 lg:m-0">
-          <span className="block text-sm text-primary">
-            Block: {blockMatch.block_number}
-          </span>
-          <span className="block text-sm text-primary">
-            Hash: <span className="block truncate">{blockMatch.hash}</span>
-          </span>
+      {!!query.length && (
+        <div className="absolute inset-x-0 top-0 -z-10 flex h-fit flex-col rounded-b-2xl rounded-t-3xl border border-primary bg-background bg-gradient-to-b from-white/[0.06] to-white/[0.12] px-2 pb-2 pt-[3.5rem] lg:m-0">
+          {blockMatch ? (
+            <Link
+              href={`/block/${blockMatch[isHash(query) ? "hash" : "block_number"]}`}
+              className="rounded-lg border border-primary-light bg-background-active p-2"
+              onClick={() => setQuery("")}
+            >
+              <div className="flex justify-between">
+                <span className="block text-sm text-primary">
+                  <span className="font-body">Block: </span>
+                  {blockMatch.block_number}
+                </span>
+                <span className="block text-sm text-primary">
+                  <span className="font-body">Proofs:</span>{" "}
+                  {
+                    blockMatch.proofs.filter((p) => p.proof_status === "proved")
+                      .length
+                  }
+                  <span className="text-xs text-body-secondary">
+                    /{blockMatch.proofs.length}
+                  </span>
+                </span>
+              </div>
+              <span className="block text-sm text-primary">
+                <span className="font-body">Hash: </span>
+                <span className="block truncate">{blockMatch.hash}</span>
+              </span>
+            </Link>
+          ) : (
+            <div className="rounded-lg border border-primary-light bg-background-active p-2">
+              {loading ? "Loading" : "No results"}
+            </div>
+          )}
         </div>
       )}
     </div>
