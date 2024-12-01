8e083ff9425df3c0ac928229280cba80fd3e104d
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-12-01 17:36:32 +0700

    feat: add hideEmpty prop

--- components/ProofStatus.tsx
+++ components/ProofStatus.tsx
@@ -27,60 +27,72 @@ export const ProofStatusInfo = () => (
 
 type ProofStatusProps = React.HTMLAttributes<HTMLDivElement> & {
   proofs: Proof[]
+  hideEmpty: boolean
 }
-const ProofStatus = ({ proofs, className, ...props }: ProofStatusProps) => {
+const ProofStatus = ({
+  proofs,
+  className,
+  hideEmpty,
+  ...props
+}: ProofStatusProps) => {
   const completedProofs = proofs.filter(isCompleted)
   const provingProofs = proofs.filter((p) => p.proof_status === "proving")
   const queuedProofs = proofs.filter((p) => p.proof_status === "queued")
 
   return (
-    <div
+    <figure
       className={cn("flex items-center gap-3 font-mono", className)}
       {...props}
     >
-      <div className="flex items-center gap-1">
-        <MetricInfo
-          trigger={
-            <div className="flex flex-nowrap items-center gap-1">
-              <StatusIcon status="proved" />
-              <span className="block">{completedProofs.length}</span>
-            </div>
-          }
-        >
-          <span className="!font-body text-body">
-            Number of completed proofs that have been published for this block
-          </span>
-        </MetricInfo>
-      </div>
-      <div className="flex items-center gap-1">
-        <MetricInfo
-          trigger={
-            <div className="flex flex-nowrap items-center gap-1">
-              <StatusIcon status="proving" />
-              <span className="block">{provingProofs.length}</span>
-            </div>
-          }
-        >
-          <span className="!font-body text-body">
-            Number of provers currently generating proofs for this block
-          </span>
-        </MetricInfo>
-      </div>
-      <div className="flex items-center gap-1">
-        <MetricInfo
-          trigger={
-            <div className="flex flex-nowrap items-center gap-1">
-              <StatusIcon status="queued" />
-              <span className="block">{queuedProofs.length}</span>
-            </div>
-          }
-        >
-          <span className="!font-body text-body">
-            Number of provers who have indicated intent to prove this block
-          </span>
-        </MetricInfo>
-      </div>
-    </div>
+      {(completedProofs.length > 0 || !hideEmpty) && (
+        <div className="flex items-center gap-1">
+          <MetricInfo
+            trigger={
+              <div className="flex flex-nowrap items-center gap-1">
+                <StatusIcon status="proved" />
+                <span className="block">{completedProofs.length}</span>
+              </div>
+            }
+          >
+            <span className="!font-body text-body">
+              Number of completed proofs that have been published for this block
+            </span>
+          </MetricInfo>
+        </div>
+      )}
+      {(provingProofs.length > 0 || !hideEmpty) && (
+        <div className="flex items-center gap-1">
+          <MetricInfo
+            trigger={
+              <div className="flex flex-nowrap items-center gap-1">
+                <StatusIcon status="proving" />
+                <span className="block">{provingProofs.length}</span>
+              </div>
+            }
+          >
+            <span className="!font-body text-body">
+              Number of provers currently generating proofs for this block
+            </span>
+          </MetricInfo>
+        </div>
+      )}
+      {(queuedProofs.length > 0 || !hideEmpty) && (
+        <div className="flex items-center gap-1">
+          <MetricInfo
+            trigger={
+              <div className="flex flex-nowrap items-center gap-1">
+                <StatusIcon status="queued" />
+                <span className="block">{queuedProofs.length}</span>
+              </div>
+            }
+          >
+            <span className="!font-body text-body">
+              Number of provers who have indicated intent to prove this block
+            </span>
+          </MetricInfo>
+        </div>
+      )}
+    </figure>
   )
 }
 
