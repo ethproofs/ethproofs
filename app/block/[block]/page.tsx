87911bfc1c769b92a330cb0ca55d008e2c2ab3ca
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-12-01 14:49:10 +0700

    linting: format

--- app/block/[block]/page.tsx
+++ app/block/[block]/page.tsx
@@ -313,7 +313,7 @@ export default async function BlockDetailsPage({
     <div className="space-y-20">
       <HeroSection>
         <HeroTitle>
-          <Box strokeWidth="1" className="text-6xl text-primary shrink-0" />
+          <Box strokeWidth="1" className="shrink-0 text-6xl text-primary" />
           <h1 className="font-mono">
             <p className="text-sm font-normal md:text-lg">Block Height</p>
             <p className="text-3xl font-semibold tracking-wide">
