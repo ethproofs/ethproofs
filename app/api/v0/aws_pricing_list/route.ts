ffbb049b8f5cc359aacc53daf5ed90ae7502f888
Pablo Pettinari <pettinarip@gmail.com>
2024-12-03 17:50:47 +0100

    add new endpoint to fetch aws_pricing_list

---
+++ app/api/v0/aws_pricing_list/route.ts
@@ -0,0 +1,16 @@
+import { createClient } from "@/utils/supabase/server"
+
+export const GET = async () => {
+  const client = createClient()
+
+  const { data, error } = await client
+    .from("aws_instance_pricing")
+    .select()
+
+  if (error) {
+    console.error("error fetching aws pricing list", error)
+    return new Response("Internal server error", { status: 500 })
+  }
+
+  return Response.json(data)
+}
