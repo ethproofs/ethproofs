a00c944a485045f36dd21ebef715051a8669a6da
Pablo Pettinari <pettinarip@gmail.com>
2024-12-06 16:50:35 +0100

    update routes and schemas

--- lib/zod/schemas/cluster.ts
+++ lib/zod/schemas/cluster.ts
@@ -1,37 +1,37 @@
 import z from ".."
 
 export const ClusterSchema = z.object({
-  cluster_id: z.number(),
-  cluster_name: z.string(),
-  cluster_description: z.string().optional(),
-  cluster_hardware: z.string().optional(),
-  cluster_cycle_type: z.string().optional(),
-  cluster_proof_type: z.string().optional(),
+  index: z.number(),
+  nickname: z.string(),
+  description: z.string().optional(),
+  hardware: z.string().optional(),
+  cycle_type: z.string().optional(),
+  proof_type: z.string().optional(),
 })
 
 export const createClusterSchema = z.object({
-  cluster_name: z.string().max(50).openapi({
+  nickname: z.string().max(50).openapi({
     description: "Human-readable name. Main display name in the UI",
     example: "ZKnight-01",
   }),
-  cluster_description: z.string().max(200).optional().openapi({
+  description: z.string().max(200).optional().openapi({
     description: "Description of the cluster",
     example: "Primary RISC-V prover",
   }),
-  cluster_hardware: z.string().max(200).optional().openapi({
+  hardware: z.string().max(200).optional().openapi({
     description: "Technical specifications",
     example: "RISC-V Prover",
   }),
-  cluster_cycle_type: z.string().max(50).optional().openapi({
+  cycle_type: z.string().max(50).optional().openapi({
     description: "Type of cycle",
     example: "SP1",
   }),
-  cluster_proof_type: z.string().max(50).optional().openapi({
+  proof_type: z.string().max(50).optional().openapi({
     description:
       "Proof system used to generate proofs. (e.g., Groth16 or PlonK)",
     example: "Groth16",
   }),
-  cluster_configuration: z
+  configuration: z
     .array(
       z.object({
         instance_type: z.string().openapi({
