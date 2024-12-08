923c0b55203773e403ccbdf37a52d49c9bef0767
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-12-08 13:30:47 +0700

    fix: clusters naming
    Gets name from db table, "clusters" not "cluster", fixes missing cluster information

--- components/BlocksTable/ClusterDetails.tsx
+++ components/BlocksTable/ClusterDetails.tsx
@@ -1,16 +1,16 @@
 import type { Proof } from "@/lib/types"
 
 const ClusterDetails = ({ proof }: { proof: Proof }) => {
-  const { cluster_id, cluster } = proof
+  const { cluster_id, clusters } = proof
 
-  if (!cluster)
+  if (!clusters)
     return (
       <span className="block text-lg font-semibold">
         Cluster {cluster_id.split("-")[0]}
       </span>
     )
 
-  const { description, hardware, nickname } = cluster
+  const { description, hardware, nickname } = clusters
 
   return (
     <div className="text-wrap">
