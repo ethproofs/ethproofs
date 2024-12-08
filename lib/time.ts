5c05ac0c6b3a932b46156ca7fbaaf96b9b1f66b8
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-12-08 16:00:42 +0700

    feat: enable prettyMilliseconds options

--- lib/time.ts
+++ lib/time.ts
@@ -1,4 +1,4 @@
-import prettyMilliseconds from "pretty-ms"
+import prettyMilliseconds, { type Options } from "pretty-ms"
 
 /**
  * Formats a duration in milliseconds to a human-readable format.
@@ -6,9 +6,23 @@ import prettyMilliseconds from "pretty-ms"
  * @param {number} milliseconds - The duration in milliseconds.
  * @returns {string} - The duration in a human-readable format.
  */
-export const prettyMs = (milliseconds: number): string => {
+export const prettyMs = (milliseconds: number, options?: Options): string => {
   if (milliseconds < 0) return "-"
-  return prettyMilliseconds(milliseconds)
+
+  const isSubMinute = milliseconds < 60_0000
+  const isSubSecond = milliseconds < 1_000
+  const isSubMillisecond = milliseconds < 1
+
+  const defaultOptions: Options = {
+    separateMilliseconds: isSubSecond,
+    formatSubMilliseconds: isSubMillisecond,
+    unitCount: isSubMinute ? 1 : 2,
+    keepDecimalsOnWholeSeconds: true,
+    secondsDecimalDigits: isSubMinute ? 1 : 0,
+    millisecondsDecimalDigits: isSubMillisecond ? 1 : 0,
+  }
+
+  return prettyMilliseconds(milliseconds, { ...defaultOptions, ...options })
 }
 
 export const getTime = (d: string): number => new Date(d).getTime()
