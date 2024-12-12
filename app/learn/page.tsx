94ed7292255e7fd899a7319734e46c049bffff19
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-12-12 13:50:10 +0700

    feat: add Overview to learn page

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 import { cn } from "@/lib/utils"
 
@@ -37,101 +38,334 @@ export default function LearnPage() {
         </p>
       </div>
 
-      <section className="space-y-16 md:mt-16 lg:mt-32 xl:mt-48">
+      <section className="mx-auto max-w-screen-md space-y-16 md:mt-16 lg:mt-32 xl:mt-48">
         <div>
-          <h1 className="text-4xl md:text-5xl">
-            Learn about SNARKifying Ethereum
-          </h1>
+          <h2 className="text-4xl md:text-5xl">Overview</h2>
           <div className="h-px w-full bg-gradient-to-r from-primary"></div>
         </div>
-        <div className="max-w-screen-lg">
-          <p>
-            This is proof of concept that should improve over time, eventually
-            proving all Ethereum blocks with reasonable costs and latency.
-          </p>
-          <p>
-            Proofs validate each Ethereum block, like a stamp of authenticity.
-            This unlocks faster, more secure transactions, ensuring both
-            efficiency and privacy, paving the way for a stronger Ethereum
-            network. Its a foundation for a new class of applications and
-            clients and acts as a major step towards a fully enshrined Ethereum.
-          </p>
-          <p>Eventually, this project can support multiple proof systems.</p>
-        </div>
-      </section>
-
-      <section className="max-w-screen-lg space-y-6">
-        <h2 className="text-2xl md:text-3xl">
-          Potential benefits and use cases
-        </h2>
-        <ol className="space-y-6">
-          <li>
-            <strong>
-              Enabling full ZK light clients of Ethereum to run on low powered
-              devices (eg. smartphones).
-            </strong>{" "}
-            Syncing with the network can be done via a single ZK proof
-            verification (once we combine Zeth ZK proofs with Sync Committee ZK
-            proofs).
-          </li>
-          <li>
-            <strong>Reduce the time and resources</strong> necessary for a new
-            node or wallet to participate in the Ethereum network.
-          </li>
-          <li>
-            <strong>Validate any current or historic Ethereum state</strong>{" "}
-            with a single proof.
-          </li>
-          <li>
-            <strong>
-              Lay the groundwork for multi-client & multi-proof ZK proofs of
+        <div className="space-y-16">
+          <div className="space-y-8">
+            <h3 className="text-3xl">What are zkEVM?</h3>
+            <p>
+              zkEVM, or Zero-Knowledge Ethereum Virtual Machine, is an advanced
+              version of the Ethereum Virtual Machine (EVM) that utilizes
+              zero-knowledge proofs to enhance privacy and scalability in
+              executing smart contracts. It allows developers to run
+              computations off-chain while submitting only proofs of their
+              validity to the Ethereum mainnet, ensuring that sensitive data
+              remains confidential.
+            </p>
+            <p>
+              By integrating zero-knowledge technology, zkEVM enables verifiable
+              transactions without exposing underlying information, making it a
+              crucial component of Layer 2 scaling solutions like zk-rollups.
+              This innovation is pivotal for improving Ethereum’s throughput and
+              accommodating a broader range of decentralized applications
+              (dApps) while maintaining security and privacy.
+            </p>
+          </div>
+          <div className="space-y-8">
+            <h3 className="text-3xl">What are the proofs?</h3>
+            <p>
+              Proofs validate each Ethereum block like a stamp of authenticity.
+              This unlocks faster, more secure transactions, ensuring efficiency
+              and privacy and paving the way for a stronger Ethereum network.
+              Proofs are a foundation for a new class of applications and
+              clients and act as a major step towards a fully enshrined
               Ethereum.
-            </strong>
-          </li>
-          <li>
-            <strong>Start tests of Ethereum enshrinement.</strong> These proofs
-            will provide a large corpus of data for further research into
-            performance improvements, gas/proving fee schedules, and more for
-            the larger ZK community.
-          </li>
-          <li>
-            <strong>Empower the Ethereum Ecosystem</strong> to take this
-            powerful new ZK technology
-          </li>
-        </ol>
-      </section>
-      <section className="max-w-screen-lg space-y-6">
-        <h2 className="text-2xl md:text-3xl">SNARKs</h2>
-        <p>
-          SNARK stands for{" "}
-          <strong>Succinct Non-interactive ARgument of Knowledge</strong>. It is
-          a cryptographic proof that allows one party to demonstrate to another
-          party that they know a particular piece of information without
-          revealing the information itself.
-        </p>
-        <p>Key features of SNARKs:</p>
-        <ol className="space-y-6">
-          <li>
-            <strong>Succinct</strong>: The proofs are very short and can be
-            verified quickly, even if the original computation is complex.
-          </li>
-          <li>
-            <strong>Non-interactive</strong>: The proof is provided in a single
-            communication, with no need for multiple rounds of exchange between
-            the prover and verifier.
-          </li>
-          <li>
-            <strong>Argument of Knowledge</strong>: The prover demonstrates that
-            they actually possess knowledge of the solution, not just that a
-            solution exists.
-          </li>
-        </ol>
-
-        <p>
-          In blockchain and Ethereum, SNARKs can be used to verify transactions
-          or computations off-chain, improving scalability and privacy by
-          reducing the amount of data that needs to be processed on-chain.
-        </p>
+            </p>
+          </div>
+          <div className="space-y-8">
+            <h3 className="text-3xl">What are SNARKs?</h3>
+            <p>
+              SNARK stands for Succinct Non-interactive ARgument of Knowledge.
+              It is a cryptographic proof that allows one party to demonstrate
+              to another party that they know a particular piece of information
+              without revealing the information itself.
+            </p>
+            <div className="space-y-4">
+              <p>Key features of SNARKs:</p>
+              <ul>
+                <li>
+                  Succinct: The proofs are very short and can be verified
+                  quickly, even if the original computation is complex.
+                </li>
+                <li>
+                  Non-interactive: The proof is provided in a single
+                  communication, with no need for multiple rounds of exchange
+                  between the prover and verifier.
+                </li>
+                <li>
+                  Argument of Knowledge: The prover demonstrates that they
+                  actually possess knowledge of the solution, not just that a
+                  solution exists.
+                </li>
+              </ul>
+            </div>
+            <p>
+              In blockchain and Ethereum, SNARKs can be used to verify
+              transactions or computations off-chain, improving scalability and
+              privacy by reducing the amount of data that needs to be processed
+              on-chain.
+            </p>
+          </div>
+          <div className="space-y-8">
+            <h3 className="text-3xl">Potential benefits and use cases</h3>
+            <ul>
+              <li>
+                Enabling full ZK light clients of Ethereum to run on low powered
+                devices (eg. smartphones). Syncing with the network can be done
+                via a single ZK proof verification (once we combine Zeth ZK
+                proofs with Sync Committee ZK proofs).
+              </li>
+              <li>
+                Reduce the time and resources necessary for a new node or wallet
+                to participate in the Ethereum network.
+              </li>
+              <li>
+                Validate any current or historic Ethereum state with a single
+                proof.
+              </li>
+              <li>
+                Lay the groundwork for multi-client & multi-proof ZK proofs of
+                Ethereum.
+              </li>
+              <li>
+                Start tests of Ethereum enshrinement. These proofs will provide
+                a large corpus of data for further research into performance
+                improvements, gas/proving fee schedules, and more for the larger
+                ZK community.
+              </li>
+              <li>
+                Empower the Ethereum Ecosystem to take this powerful new ZK
+                technology
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-8">
+            <h3 className="text-3xl">More resources</h3>
+            <ul>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://www.lita.foundation/blog/zero-knowledge-paradigm-zkvm"
+                >
+                  a zero-knowledge paradigm series
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://www.youtube.com/watch?v=vVgHL5vpJxY&t=33s"
+                >
+                  cairo – a turing-complete stark-friendly cpu architecture -
+                  shahar papini
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://youtube.com/playlist?list=PLjQ9HCQMu_8xjOEM_vh5p26ODtr-mmGxO&si=Uega8IMg_J8kNaa8"
+                >
+                  lasso + jolt playlist
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://www.mikkoikola.com/blog/2023/12/11/new-paradigm-in-ethereum-l2-scaling-multi-proving-and-zk-vms"
+                >
+                  new paradigm in ethereum l2 scaling: multi-proving and zk-vms
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://www.youtube.com/watch?v=UtzFOwQp8n4"
+                >
+                  the nexus v1.0 zkvm - daniel marin (nexus)
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://a16zcrypto.com/posts/article/understanding-jolt-clarifications-and-reflections/"
+                >
+                  understanding jolt: clarifications and reflections
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://www.youtube.com/watch?v=GRFPGJW0hic"
+                >
+                  zk whiteboard sessions – module seven: zero knowledge virtual
+                  machines (zkvm) with grjte
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://www.youtube.com/watch?v=tWJZX-WmbeY&t=325s"
+                >
+                  zk10: analysis of zkvm designs - wei dai & terry chung
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://www.youtube.com/watch?v=HDH2KXRAxAc"
+                >
+                  zk11: o1vm: building a real-world zkvm for mips - danny
+                  willems
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://www.youtube.com/watch?v=kzSYNFh4uQ0&list=PLothk45x3HC9Oz4f3e9-OoYUEytfHWCl5"
+                >
+                  zk12: memory checking in ivc-based zkvm - jens groth
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://www.youtube.com/watch?v=81UAaiIgIYA&t=803s"
+                >
+                  zk7: miden vm: a stark-friendly vm for blockchains - bobbin
+                  threadbare – polygon
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://taiko.mirror.xyz/e_5GeGGFJIrOxqvXOfzY6HmWcRjCjRyG0NQF1zbNpNQ"
+                >
+                  zeroing into zkvm
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://www.youtube.com/watch?v=aobrJ-zTcAU"
+                >
+                  zkvm design walkthrough with max and daniel
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://github.com/CertiKProject/zkwasm-fv"
+                >
+                  Verification of zkWasm in Coq
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://www.youtube.com/watch?v=R07ina4k7hg"
+                >
+                  zk11: polynomial acceleration for stark vms
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://www.youtube.com/watch?v=11DIflEwx50"
+                >
+                  what does risc v have to do with risc zero's zkvm
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://www.youtube.com/watch?v=RtGk6967PC4"
+                >
+                  risc zero architecture presentation @ stanford
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://www.youtube.com/watch?v=h1qWnf-M5lo"
+                >
+                  continuations: scaling in zkvm
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://a16zcrypto.com/posts/article/getting-bugs-out-of-snarks/"
+                >
+                  Getting the bugs out of SNARKs: The road ahead
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://www.youtube.com/watch?v=zD45V6GAD00"
+                >
+                  ~tacryt-socryp on Zorp, the Nock zkVM | Reassembly23
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-8">
+            <h3 className="text-3xl">Tutorials</h3>
+            <ul>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://neptune.cash/learn/brainfuck-tutorial/"
+                >
+                  brainfuck tutorial
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://blog.lambdaclass.com/continuous-read-only-memory-constraints-an-implementation-using-lambdaworks/"
+                >
+                  continuous read only memory constraints an implementation
+                  using lambdaworks
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://blog.lambdaclass.com/how-to-code-fri-from-scratch/"
+                >
+                  fri from scratch
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://dev.risczero.com/proof-system/stark-by-hand"
+                >
+                  stark by hand
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://aszepieniec.github.io/stark-brainfuck/"
+                >
+                  stark brainfuck
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="hover:text-primary-light"
+                  href="https://starkware.co/stark-101/"
+                >
+                  stark 101
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </section>
     </div>
   )
