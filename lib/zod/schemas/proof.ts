7b843b9e439afbd85c0eb7234698e45b5de5ede3
Joshua <62268199+minimalsm@users.noreply.github.com>
2024-12-13 02:41:27 +0000

    Update validation to prevent OOM error

--- lib/zod/schemas/proof.ts
+++ lib/zod/schemas/proof.ts
@@ -18,18 +18,22 @@ export const provingProofSchema = baseProofSchema.extend({})
 export const provedProofSchema = baseProofSchema.extend({
   proving_time: z
     .number()
-    .positive("proving_time must be a positive number")
-    .describe("Milliseconds taken to generate the proof"),
+    .positive("proving_time must be a positive number"),
   proving_cycles: z
     .number()
     .int()
     .positive("proving_cycles must be a positive integer")
-    .optional()
-    .describe("Number of cycles taken to generate the proof"),
+    .optional(),
   proof: z
     .string()
-    .base64()
     .min(1, "proof is required for 'proved' status")
-    .describe("Proof in base64 format"),
-  verifier_id: z.string().optional().describe("vkey/image-id"),
-})
+    .refine((val) => isValidBase64(val), { message: "proof must be a valid base64 string" }),
+  verifier_id: z.string().optional(),
+});
+
+// validate base64 without decoding
+// decoding with zod causing out of memory errors: https://github.com/ethproofs/ethproofs/pull/154
+const isValidBase64 = (str: string) => {
+  const base64Regex = /^[A-Za-z0-9+/]+={0,2}$/;
+  return base64Regex.test(str);
+};
