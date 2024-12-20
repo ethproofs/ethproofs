0df5c920d8bc76dbbbd05e508a85981965c5d7da
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-12-20 17:27:02 -0500

    refactor: implement NotFoundTemplate

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,20 +1,5 @@
-import { ButtonLink } from "@/components/ui/button"
-import { HeroSection } from "@/components/ui/hero"
+import NotFoundTemplate from "@/components/NotFoundTemplate"
 
 export default async function NotFound() {
-  return (
-    <>
-      <HeroSection className="space-y-4">
-        <h1 className="flex flex-col items-center gap-4 font-mono text-5xl md:flex-row">
-          404
-        </h1>
-        <p className="text-center font-mono md:text-start">Page not found</p>
-      </HeroSection>
-      <div className="justify-center p-6 max-md:flex">
-        <ButtonLink href="/" className="mx-auto h-fit w-fit" size="lg">
-          View all proofs
-        </ButtonLink>
-      </div>
-    </>
-  )
+  return <NotFoundTemplate />
 }
