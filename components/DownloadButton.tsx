5bb56c6e35c48aadfd1957c16539a3c909d1d382
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-12-21 20:50:15 -0500

    refactor: break apart and pass specific classes

--- components/DownloadButton.tsx
+++ components/DownloadButton.tsx
@@ -15,39 +15,47 @@ import Tooltip from "./Tooltip"
 type DownloadButtonProps = {
   proof: Proof
   className?: string
+  containerClass?: string
+  labelClass?: string
 }
 
-const DownloadButton = ({ className, proof }: DownloadButtonProps) => {
+const DownloadButton = ({
+  className,
+  proof,
+  containerClass = "flex-col",
+  labelClass,
+}: DownloadButtonProps) => {
   const { proof_status, proof_id, size_bytes, team } = proof
 
   const teamName = team?.team_name ? team.team_name : "Team"
 
-  const sizingClassName =
-    "h-8 gap-2 self-center text-2xl sm:max-md:w-40 lg:w-40"
-  const labelDisplay = "hidden sm:inline-block md:hidden lg:inline-block"
-  const labelStyle = "text-nowrap text-xs font-bold"
-
   const fakeButtonClassName =
     "bg-body-secondary/10 hover:bg-body-secondary/10 cursor-auto"
 
+  const labelClassName = cn(
+    "inline-block text-nowrap text-xs font-bold",
+    labelClass
+  )
+
+  const sizingClassName = "h-8 gap-2 self-center text-2xl"
+
   if (proof_status === "proved")
     return (
-      <div className="flex flex-col items-center">
+      <div className={cn("flex items-center gap-x-2 gap-y-1", containerClass)}>
         <Button
           variant="outline"
           className={cn(sizingClassName, className)}
+          size="icon"
           asChild
         >
           <a href={`/api/v0/proofs/download/${proof_id}`} download>
             <ArrowDown />
-            <span className={labelStyle}>
-              <span className={labelDisplay}>
-                Download {size_bytes ? "" : "proof"}
-              </span>{" "}
-              {size_bytes ? prettyBytes(size_bytes) : ""}
-            </span>
+            <span className={labelClassName}>Download proof</span>
           </a>
         </Button>
+        <span className="text-xs text-body-secondary">
+          {size_bytes ? prettyBytes(size_bytes) : ""}
+        </span>
       </div>
     )
 
@@ -57,11 +65,9 @@ const DownloadButton = ({ className, proof }: DownloadButtonProps) => {
         content={`${teamName} currently generating proof for this block`}
       >
         <Button size="icon" variant="outline" asChild>
-          <div className={cn(sizingClassName, fakeButtonClassName, className)}>
+          <div className={cn(fakeButtonClassName, sizingClassName, className)}>
             <StatusIcon status="proving" className="animate-pulse" />
-            <span
-              className={cn(labelDisplay, labelStyle, "text-body-secondary")}
-            >
+            <span className={cn(labelClassName, "text-body-secondary")}>
               Proving
             </span>
           </div>
@@ -73,11 +79,9 @@ const DownloadButton = ({ className, proof }: DownloadButtonProps) => {
     return (
       <Tooltip content={`${teamName} has indicated intent to prove this block`}>
         <Button size="icon" variant="outline" asChild>
-          <div className={cn(sizingClassName, fakeButtonClassName, className)}>
+          <div className={cn(fakeButtonClassName, sizingClassName, className)}>
             <StatusIcon status="queued" />
-            <span
-              className={cn(labelDisplay, labelStyle, "text-body-secondary")}
-            >
+            <span className={cn(labelClassName, "text-body-secondary")}>
               Queued
             </span>
           </div>
