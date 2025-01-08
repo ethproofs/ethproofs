38f33446fef8e68fa1801297f3ef77458a283522
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-01-07 18:39:38 -0800

    feat: create download/all api route

---
+++ app/api/v0/proofs/download/all/[block]/route.ts
@@ -0,0 +1,66 @@
+import AdmZip from "adm-zip"
+
+import { db } from "@/db"
+
+export async function GET(
+  _request: Request,
+  { params }: { params: { block: string } }
+) {
+  const { block } = params
+
+  const proofRows = await db.query.proofs.findMany({
+    columns: {
+      block_number: true,
+      cluster_id: true,
+      team_id: true,
+      proof_id: true,
+    },
+    with: {
+      proof_binary: true,
+    },
+    where: (proofs, { eq }) => eq(proofs.block_number, Number(block)),
+  })
+
+  if (!proofRows || !proofRows.length) {
+    return new Response("No proofs found", { status: 404 })
+  }
+
+  const binaryBuffers: { binaryBuffer: Buffer; filename: string }[] = []
+
+  for (const proofRow of proofRows) {
+    const team = await db.query.teams.findFirst({
+      columns: {
+        name: true,
+      },
+      where: (teams, { eq }) => eq(teams.id, proofRow.team_id),
+    })
+
+    const teamName = team?.name ? team.name : proofRow.cluster_id.split("-")[0]
+    const filename = `block_${block}_proof_${proofRow.proof_id}_${teamName}.txt`
+
+    const binaryBuffer = Buffer.from(
+      proofRow.proof_binary.proof_binary.slice(2),
+      "hex"
+    )
+
+    binaryBuffers.push({ binaryBuffer, filename })
+  }
+
+  console.log(`Adding ${binaryBuffers.length} files to the zip`)
+
+  // Create a zip file from buffers using adm-zip
+  const zip = new AdmZip()
+  binaryBuffers.forEach(({ binaryBuffer, filename }) => {
+    console.log(`Adding file: ${filename}`)
+    zip.addFile(filename, binaryBuffer)
+  })
+
+  const zipBuffer = zip.toBuffer()
+
+  return new Response(zipBuffer, {
+    headers: {
+      "Content-Type": "application/zip",
+      "Content-Disposition": `attachment; filename="block_${block}_all_proofs.zip"`,
+    },
+  })
+}
