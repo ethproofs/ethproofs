a5b5b7e6d0cb395e3566ea5ead02fa729ec2d4ee
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-01-08 12:24:15 -0800

    feat: add root not-found page
    per Figma

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,17 @@
-import NotFoundTemplate from "@/components/NotFoundTemplate"
+import TriangleAlert from "@/components/svgs/triangle-alert.svg"
+import { ButtonLink } from "@/components/ui/button"
+import { Divider } from "@/components/ui/divider"
 
 export default async function NotFound() {
-  return <NotFoundTemplate />
+  return (
+    <div className="flex flex-col items-center gap-4 text-center">
+      <TriangleAlert className="size-40 stroke-[1.25px] text-primary" />
+      <h1 className="mb-4 font-mono font-normal text-primary">404</h1>
+      <p className="text-lg">Nothing here</p>
+      <Divider className="my-6" />
+      <ButtonLink href="/" variant="outline" size="lg">
+        Back to homepage
+      </ButtonLink>
+    </div>
+  )
 }
