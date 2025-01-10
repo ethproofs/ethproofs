8638079585ec085f0fa366ace86b398b708de700
Pablo <pettinarip@gmail.com>
2025-01-10 17:44:57 +0100

    handle unhandled errors and reports

--- lib/middleware/with-telemetry.ts
+++ lib/middleware/with-telemetry.ts
@@ -1,12 +1,57 @@
+type Report = {
+  date: string
+  startTime: number
+  endTime: number
+  durationMs: number
+  errorMessage?: string
+}
+
+const logReport = (message: string, report: Report) => {
+  console.log(message)
+  console.log(report)
+
+  if (report.errorMessage) {
+    // send error to discord
+    console.log("sending error to discord")
+  }
+}
+
 export const withTelemetry = (
   handler: (request: Request) => Promise<Response>
 ) => {
   return async (request: Request) => {
-    const startTime = new Date()
-    const response = await handler(request)
-    const endTime = new Date()
-    const duration = endTime.getTime() - startTime.getTime()
-    console.log(`Request duration: ${duration}ms`)
-    return response
+    const report: Report = {
+      date: new Date().toISOString(),
+      startTime: new Date().getTime(),
+      endTime: new Date().getTime(),
+      durationMs: 0,
+    }
+
+    try {
+      const response = await handler(request)
+
+      report.endTime = new Date().getTime()
+      report.durationMs = report.endTime - report.startTime
+      logReport(
+        `${request.method} ${request.url} ${response.status} in ${report.durationMs}ms ${report.date}`,
+        report
+      )
+      return response
+    } catch (error) {
+      report.endTime = new Date().getTime()
+      report.durationMs = report.endTime - report.startTime
+
+      if (error instanceof Error) {
+        report.errorMessage = error.message
+      }
+
+      logReport(
+        `[unhandled error] ${request.method} ${request.url} 500 in ${report.durationMs}ms ${report.date}`,
+        report
+      )
+
+      // forward error to next handler
+      throw error
+    }
   }
 }
