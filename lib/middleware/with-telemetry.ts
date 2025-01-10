6bb84a1a9654219de3627fbe3fa71edd486d97e6
Pablo <pettinarip@gmail.com>
2025-01-10 20:06:45 +0100

    send report to telegram

--- lib/middleware/with-telemetry.ts
+++ lib/middleware/with-telemetry.ts
@@ -1,18 +1,38 @@
+import { md } from "@vlad-yakovlev/telegram-md"
+
 type Report = {
   date: string
   startTime: number
   endTime: number
   durationMs: number
+  statusCode?: number
   errorMessage?: string
 }
 
-const logReport = (message: string, report: Report) => {
-  console.log(message)
-  console.log(report)
+const sendReport = async (message: string, report: Report) => {
+  const formattedReport = JSON.stringify(report, null, 4)
+  const text = md`
+    ${md.bold(message)}
+    ${md.codeBlock(formattedReport, "json")}
+  `
 
-  if (report.errorMessage) {
-    // send error to discord
-    console.log("sending error to discord")
+  // send to telegram
+  const telegramUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`
+  const response = await fetch(telegramUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: process.env.TELEGRAM_CHAT_ID,
+      text: md.build(text),
+      parse_mode: "MarkdownV2",
+    }),
+  })
+
+  if (!response.ok) {
+    const error = await response.json()
+    console.error("Failed to send report to telegram", error)
   }
 }
 
@@ -32,23 +52,30 @@ export const withTelemetry = (
 
       report.endTime = new Date().getTime()
       report.durationMs = report.endTime - report.startTime
-      logReport(
-        `${request.method} ${request.url} ${response.status} in ${report.durationMs}ms ${report.date}`,
-        report
-      )
+      report.statusCode = response.status
+
+      const message = `${request.method} ${request.url} ${report.statusCode} in ${report.durationMs}ms ${report.date}`
+      if (report.statusCode !== 200) {
+        sendReport(message, report)
+      }
+
+      console.log(message)
+
       return response
     } catch (error) {
       report.endTime = new Date().getTime()
       report.durationMs = report.endTime - report.startTime
+      report.statusCode = 500
 
       if (error instanceof Error) {
         report.errorMessage = error.message
       }
 
-      logReport(
-        `[unhandled error] ${request.method} ${request.url} 500 in ${report.durationMs}ms ${report.date}`,
-        report
-      )
+      const message = `[unhandled error] ${request.method} ${request.url} ${report.statusCode} in ${report.durationMs}ms ${report.date}`
+
+      sendReport(message, report)
+
+      console.log(message)
 
       // forward error to next handler
       throw error
