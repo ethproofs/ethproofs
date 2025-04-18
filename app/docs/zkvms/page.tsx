e907331ed5c694b09467362a8d803bbd90d315e3
Pablo <pettinarip@gmail.com>
2025-04-18 13:41:57 +0200

    update copy in zkvms page

--- app/docs/zkvms/page.tsx
+++ app/docs/zkvms/page.tsx
@@ -1,11 +1,21 @@
 import Link from "@/components/ui/link"
 
+import { SITE_REPO } from "@/lib/constants"
+
 import { ZkvmsTable } from "./zkvms-table"
 
 import { getZkvms } from "@/lib/api/zkvms"
 
 export default async function ZkvmsPage() {
   const zkvms = await getZkvms()
+  const repoUrl = new URL(SITE_REPO, "https://github.com")
+
+  const issuesUrl = new URL(`${repoUrl}/issues/new`, "https://github.com")
+  issuesUrl.searchParams.set("title", "Add a new zkVM")
+  issuesUrl.searchParams.set(
+    "body",
+    "Please add the following zkVM to the list:"
+  )
 
   return (
     <div className="flex flex-col gap-8">
@@ -31,6 +41,12 @@ export default async function ZkvmsPage() {
       </div>
 
       <ZkvmsTable zkvms={zkvms} />
+
+      <p className="text-muted-foreground text-sm">
+        If you&apos;re looking for a zkVM version that&apos;s not listed here,
+        please <Link href={issuesUrl.toString()}>open a GitHub issue</Link> and
+        we&apos;ll work on adding it to the list.
+      </p>
     </div>
   )
 }
