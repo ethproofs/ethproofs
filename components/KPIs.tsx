9b7ba03001b65c7cb474146fa3085e2acfede598
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-04-21 21:45:38 -0600

    feat: create KPI component

---
+++ components/KPIs.tsx
@@ -0,0 +1,33 @@
+import { SummaryItem } from "@/lib/types"
+
+export const KPI = ({
+  item: { icon, value, label },
+}: {
+  item: SummaryItem
+}) => (
+  <div className="row-span-2 grid grid-rows-subgrid gap-1 p-2">
+    {/* Row 1 - Metric icon and value */}
+    <div className="col-span-1 row-span-1 flex h-full flex-col items-center justify-center gap-x-2 md:flex-row">
+      <p className="font-mono text-2xl text-primary md:text-3xl lg:text-4xl">
+        {icon}
+      </p>
+      <p className="h-auto text-nowrap text-center font-mono text-2xl text-primary md:text-3xl lg:text-4xl">
+        {value}
+      </p>
+    </div>
+    {/* Row 2 - Metric label */}
+    <div>
+      <p className="text-center text-xs uppercase md:text-sm">{label}</p>
+    </div>
+  </div>
+)
+
+const KPIs = ({ items }: { items: SummaryItem[] }) => (
+  <div className="mx-auto grid w-full max-w-2xl grid-cols-[auto,auto,auto] gap-4">
+    {items.map((item) => (
+      <KPI key={item.key} item={item} />
+    ))}
+  </div>
+)
+
+export default KPIs
