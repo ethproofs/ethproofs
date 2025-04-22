3df4be49d489da91d79931064acb44f6a0084911
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-04-21 21:08:03 -0600

    feat: iterate zkvm accordion, use subgrid

--- components/ZkvmAccordion.tsx
+++ components/ZkvmAccordion.tsx
@@ -10,26 +10,45 @@ import {
 } from "./ui/accordion"
 
 import SuccinctLogo from "@/components/svgs/succinct-logo.svg"
+import { AccordionItemProps } from "@radix-ui/react-accordion"
 
-const ZkvmAccordion = () => (
-  <Accordion type="multiple">
-    <AccordionItem value="item-1 w-full">
-      <div className="flex justify-between border-b">
-        <div className="flex items-center gap-3">
-          <span className="block font-mono text-2xl text-primary">SP1</span>
-          <span className="block font-mono text-sm italic text-body-secondary">
-            by
-          </span>
-          <SuccinctLogo />
-        </div>
-        <AccordionTrigger className="my-2 h-fit gap-2 rounded-full border-2 border-primary-border bg-background-highlight p-0.5 text-primary [&>svg]:size-6">
-          <Pizza slices={DEMO_SLICES} disableEffects />
-        </AccordionTrigger>
+const ZkvmAccordionItem = ({ value }: Pick<AccordionItemProps, "value">) => (
+  <AccordionItem value={value} className="col-span-5 grid grid-cols-subgrid">
+    <div className="col-span-5 grid grid-cols-subgrid items-center gap-12 border-b">
+      <div className="col-start-1 flex items-center gap-3">
+        <span className="block font-mono text-2xl text-primary">SP1</span>
+        <span className="block font-mono text-sm italic text-body-secondary">
+          by
+        </span>
+        <SuccinctLogo />
       </div>
-      <AccordionContent className="border-b p-0">
-        <ZkvmDetails />
-      </AccordionContent>
-    </AccordionItem>
+      <div id="version" className="col-start-2">
+        4.20
+      </div>
+      <div id="isa" className="col-start-3">
+        name
+      </div>
+      <div id="used-by" className="col-start-4">
+        5/<span className="text-xs">12</span>
+      </div>
+
+      <AccordionTrigger className="col-start-5 my-2 h-fit gap-2 rounded-full border-2 border-primary-border bg-background-highlight p-0.5 text-primary [&>svg]:size-6">
+        <Pizza slices={DEMO_SLICES} disableEffects />
+      </AccordionTrigger>
+    </div>
+    <AccordionContent className="col-span-full border-b p-0">
+      <ZkvmDetails />
+    </AccordionContent>
+  </AccordionItem>
+)
+const ZkvmAccordion = () => (
+  <Accordion
+    type="multiple"
+    className="grid w-full grid-cols-[1fr_repeat(4,_auto)]"
+  >
+    {Array.from({ length: 12 }, (_, i) => (
+      <ZkvmAccordionItem key={i} value={"item-" + i} />
+    ))}
   </Accordion>
 )
 
