63f561fb3336bdcb613a8bb1798d98a61e0a8851
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-04-24 13:44:14 -0600

    feat: add links to sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -36,24 +36,42 @@ export function AppSidebar() {
         <SearchInput className="max-md:hidden" placeholder="Search by block" />
       </SidebarHeader>
       <SidebarContent>
-        <SidebarMenu className="space-y-10 px-6">
+        <SidebarMenu className="gap-y-10 px-6">
           <SidebarGroup className="">
             <SidebarGroupLabel className="">Home</SidebarGroupLabel>
-            <SidebarMenuItem className="">Dashboard</SidebarMenuItem>
+            <Link href="/" className="text-body hover:text-primary">
+              <SidebarMenuItem>Dashboard</SidebarMenuItem>
+            </Link>
           </SidebarGroup>
-          <SidebarGroup className="">
-            <SidebarGroupLabel className="">Explore</SidebarGroupLabel>
-            <SidebarMenuItem className="">zkVMs</SidebarMenuItem>
-            <SidebarMenuItem className="">provers</SidebarMenuItem>
-            <SidebarMenuItem className="">blocks</SidebarMenuItem>
-            <SidebarMenuItem className="">killers</SidebarMenuItem>
+          <SidebarGroup>
+            <SidebarGroupLabel>Explore</SidebarGroupLabel>
+            <Link href="/zkvms" className="text-body hover:text-primary">
+              <SidebarMenuItem>zkVMs</SidebarMenuItem>
+            </Link>
+            <Link href="/provers" className="text-body hover:text-primary">
+              <SidebarMenuItem>provers</SidebarMenuItem>
+            </Link>
+            <Link href="/blocks" className="text-body hover:text-primary">
+              <SidebarMenuItem>blocks</SidebarMenuItem>
+            </Link>
+            <Link href="/killers" className="text-body hover:text-primary">
+              <SidebarMenuItem>killers</SidebarMenuItem>
+            </Link>
           </SidebarGroup>
-          <SidebarGroup className="">
-            <SidebarGroupLabel className="">Learn</SidebarGroupLabel>
-            <SidebarMenuItem className="">zkVMs and SNARKs</SidebarMenuItem>
-            <SidebarMenuItem className="">blocks</SidebarMenuItem>
-            <SidebarMenuItem className="">API</SidebarMenuItem>
-            <SidebarMenuItem className="">about Ethproofs</SidebarMenuItem>
+          <SidebarGroup>
+            <SidebarGroupLabel>Learn</SidebarGroupLabel>
+            <Link href="/learn" className="text-body hover:text-primary">
+              <SidebarMenuItem>zkVMs and SNARKs</SidebarMenuItem>
+            </Link>
+            <Link href="/#?" className="text-body hover:text-primary">
+              <SidebarMenuItem>blocks</SidebarMenuItem>
+            </Link>
+            <Link href="/api.html" className="text-body hover:text-primary">
+              <SidebarMenuItem>API</SidebarMenuItem>
+            </Link>
+            <Link href="/#?" className="text-body hover:text-primary">
+              <SidebarMenuItem>about Ethproofs</SidebarMenuItem>
+            </Link>
           </SidebarGroup>
         </SidebarMenu>
       </SidebarContent>
