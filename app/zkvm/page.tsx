427c8c9a17d6b698b29f92dfd1f12e2e2e68b94b
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-04-25 14:51:44 -0700

    refactor: use 307 redirect page
    redirects prior to page load preventing screen flash

--- app/zkvm/page.tsx
+++ app/zkvm/page.tsx
@@ -1,14 +1,5 @@
-"use client"
+import { redirect } from "next/navigation"
 
-import { useEffect } from "react"
-import { useRouter } from "next/navigation"
-
-export default function RedirectPage() {
-  const router = useRouter()
-
-  useEffect(() => {
-    router.replace(`/zkvms`)
-  }, [router])
-
-  return null
+export default function Page() {
+  redirect("zkvms")
 }
