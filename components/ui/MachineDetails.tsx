9a0cf83671440a1b87c140e8c13463c71e39cbdb
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-05-01 21:07:40 -0700

    update: spacing

--- components/ui/MachineDetails.tsx
+++ components/ui/MachineDetails.tsx
@@ -11,16 +11,16 @@ const MachineDetails = ({
   ...props
 }: MachineDetailsProps) => (
   <div className={cn("flex flex-col gap-y-3 p-4", className)} {...props}>
-    <span className="block text-center font-mono text-lg uppercase text-primary">
+    <span className="block text-center font-mono text-lg text-primary">
       {machine.cpuModel}
     </span>
     <div className="flex flex-col items-center gap-y-3">
       <div className="flex gap-x-3 text-center text-sm">
-        <div className="flex min-w-24 flex-1 flex-col items-center">
+        <div className="flex flex-1 flex-col items-center p-2">
           <div className="text-nowrap text-body-secondary">CPU cores</div>
           <div className="">{machine.cpuCount}</div>
         </div>
-        <div className="flex min-w-24 flex-1 flex-col items-center">
+        <div className="flex flex-1 flex-col items-center p-2">
           <div className="text-nowrap text-body-secondary">CPU RAM</div>
           <div className="">{machine.cpuRam} GB</div>
         </div>
@@ -33,11 +33,11 @@ const MachineDetails = ({
             </div>
 
             <div className="flex gap-x-3 text-center text-sm">
-              <div className="flex min-w-24 flex-1 flex-col items-center">
+              <div className="flex flex-1 flex-col items-center p-2">
                 <div className="text-nowrap text-body-secondary">GPUs</div>
                 <div className="">{machine.gpuCount[index]}</div>
               </div>
-              <div className="flex min-w-24 flex-1 flex-col items-center">
+              <div className="flex flex-1 flex-col items-center p-2">
                 <div className="text-nowrap text-body-secondary">GPU RAM</div>
                 <div className="">{machine.gpuRam[index]} GB</div>
               </div>
