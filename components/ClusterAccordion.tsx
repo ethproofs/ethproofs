554616079bda81ddeeb8c0692c62e5580d56e8cc
Pablo <pettinarip@gmail.com>
2025-05-05 14:54:11 +0200

    refactor ClusterAccordion to use the latest cluster version and handle cases with no versions

--- components/ClusterAccordion.tsx
+++ components/ClusterAccordion.tsx
@@ -54,100 +54,109 @@ type ClusterAccordionItemProps = Pick<AccordionItemProps, "value"> & {
 const ClusterAccordionItem = ({
   value,
   clusterDetails,
-}: ClusterAccordionItemProps) => (
-  <AccordionItem value={value} className="col-span-6 grid grid-cols-subgrid">
-    <div className="col-span-6 grid grid-cols-subgrid items-center gap-12 px-6 py-4 hover:bg-primary/5 dark:hover:bg-primary/10">
-      {/* TODO: Update to match design */}
-      <div className="col-start-1 flex flex-col gap-1">
-        <Link
-          href={`/prover/${clusterDetails.team.id}`}
-          className="-m-1 w-fit rounded p-1 hover:bg-primary/10"
-        >
-          {clusterDetails.team.logo_url ? (
-            <Image
-              src={clusterDetails.team.logo_url}
-              alt="Prover logo"
-              height={16}
-              width={16}
-              style={{ height: "1rem", width: "auto" }}
-              className="dark:invert"
-            />
-          ) : (
-            <div className="flex items-center gap-1">
-              <div className="size-4 rounded-full bg-primary-border" />
-              {clusterDetails.team.name}
-            </div>
-          )}
-        </Link>
-        <div>
-          <span className="text-sm text-primary">
-            <Link
-              href={`/zkvm/${clusterDetails.versions[0].zkvm_version.zkvm.slug}`}
-              className="hover:underline"
-            >
-              {clusterDetails.versions[0].zkvm_version.zkvm.name}
-            </Link>{" "}
-            |{" "}
-          </span>
-          <span className="text-sm">{clusterDetails.nickname}</span>
-        </div>
-      </div>
-      <div className="col-start-2 flex justify-center">
-        {clusterDetails.is_open_source ? (
-          <Check className="text-level-best" strokeLinecap="square" />
-        ) : (
-          <RedX className="text-level-worst" strokeLinecap="square" />
-        )}
-      </div>
-      <div className="col-start-3">{formatUsd(clusterDetails.avg_cost)}</div>
-      <div className="col-start-4">{prettyMs(clusterDetails.avg_time)}</div>
+}: ClusterAccordionItemProps) => {
+  const lastVersion = clusterDetails.versions[0]
 
-      <AccordionTrigger className="col-start-6 my-2 h-fit gap-2 rounded-full border-2 border-primary-border bg-background-highlight p-0.5 text-primary [&>svg]:size-6">
-        <span className="sr-only">Toggle details</span>
-      </AccordionTrigger>
-    </div>
-    <AccordionContent className="relative col-span-full flex flex-col gap-12 p-6">
-      <div className="flex items-center gap-x-20">
-        <ClusterMachineSummary
-          machines={clusterDetails.versions[0].cluster_machines}
-        />
+  if (!lastVersion) {
+    throw new Error("No cluster version found")
+  }
 
-        <div className="flex flex-1 flex-col space-y-4 overflow-x-hidden">
-          <HardwareGrid
-            clusterMachines={clusterDetails.versions[0].cluster_machines}
-          />
-          <div className="flex items-center gap-3 self-end">
-            <span className="me-4">GPUs</span>
-            {Array(6)
-              .fill(0)
-              .map((_, i) => (
-                <Fragment key={i}>
-                  <span className="-me-1">{2 ** i}</span>
-                  <div
-                    key={i}
-                    className={cn("size-4 rounded-[4px]", getBoxIndexColor(i))}
-                  />
-                </Fragment>
-              ))}
+  return (
+    <AccordionItem value={value} className="col-span-6 grid grid-cols-subgrid">
+      <div className="col-span-6 grid grid-cols-subgrid items-center gap-12 px-6 py-4 hover:bg-primary/5 dark:hover:bg-primary/10">
+        {/* TODO: Update to match design */}
+        <div className="col-start-1 flex flex-col gap-1">
+          <Link
+            href={`/prover/${clusterDetails.team.id}`}
+            className="-m-1 w-fit rounded p-1 hover:bg-primary/10"
+          >
+            {clusterDetails.team.logo_url ? (
+              <Image
+                src={clusterDetails.team.logo_url}
+                alt="Prover logo"
+                height={16}
+                width={16}
+                style={{ height: "1rem", width: "auto" }}
+                className="dark:invert"
+              />
+            ) : (
+              <div className="flex items-center gap-1">
+                <div className="size-4 rounded-full bg-primary-border" />
+                {clusterDetails.team.name}
+              </div>
+            )}
+          </Link>
+          <div>
+            <span className="text-sm text-primary">
+              <Link
+                href={`/zkvm/${lastVersion.zkvm_version.zkvm.slug}`}
+                className="hover:underline"
+              >
+                {lastVersion.zkvm_version.zkvm.name}
+              </Link>{" "}
+              |{" "}
+            </span>
+            <span className="text-sm">{clusterDetails.nickname}</span>
           </div>
         </div>
-      </div>
+        <div className="col-start-2 flex justify-center">
+          {clusterDetails.is_open_source ? (
+            <Check className="text-level-best" strokeLinecap="square" />
+          ) : (
+            <RedX className="text-level-worst" strokeLinecap="square" />
+          )}
+        </div>
+        <div className="col-start-3">{formatUsd(clusterDetails.avg_cost)}</div>
+        <div className="col-start-4">{prettyMs(clusterDetails.avg_time)}</div>
 
-      <div className="grid place-items-center">
-        <ButtonLink variant="outline" href={`/cluster/${clusterDetails.id}`}>
-          See all details
-        </ButtonLink>
+        <AccordionTrigger className="col-start-6 my-2 h-fit gap-2 rounded-full border-2 border-primary-border bg-background-highlight p-0.5 text-primary [&>svg]:size-6">
+          <span className="sr-only">Toggle details</span>
+        </AccordionTrigger>
       </div>
+      <AccordionContent className="relative col-span-full flex flex-col gap-12 p-6">
+        <div className="flex items-center gap-x-20">
+          <ClusterMachineSummary machines={lastVersion.cluster_machines} />
 
-      <div className="absolute bottom-6 right-6">
-        <span className="text-xs italic text-body-secondary">Last updated</span>{" "}
-        <span className="text-xs uppercase text-body">
-          {formatShortDate(new Date(clusterDetails.versions[0].created_at))}
-        </span>
-      </div>
-    </AccordionContent>
-  </AccordionItem>
-)
+          <div className="flex flex-1 flex-col space-y-4 overflow-x-hidden">
+            <HardwareGrid clusterMachines={lastVersion.cluster_machines} />
+            <div className="flex items-center gap-3 self-end">
+              <span className="me-4">GPUs</span>
+              {Array(6)
+                .fill(0)
+                .map((_, i) => (
+                  <Fragment key={i}>
+                    <span className="-me-1">{2 ** i}</span>
+                    <div
+                      key={i}
+                      className={cn(
+                        "size-4 rounded-[4px]",
+                        getBoxIndexColor(i)
+                      )}
+                    />
+                  </Fragment>
+                ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="grid place-items-center">
+          <ButtonLink variant="outline" href={`/cluster/${clusterDetails.id}`}>
+            See all details
+          </ButtonLink>
+        </div>
+
+        <div className="absolute bottom-6 right-6">
+          <span className="text-xs italic text-body-secondary">
+            Last updated
+          </span>{" "}
+          <span className="text-xs uppercase text-body">
+            {formatShortDate(new Date(lastVersion.created_at))}
+          </span>
+        </div>
+      </AccordionContent>
+    </AccordionItem>
+  )
+}
 
 type ClusterAccordionProps = {
   clusters: (ClusterWithRelations & { avg_cost: number; avg_time: number })[]
