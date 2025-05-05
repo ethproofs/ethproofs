d8d27f3611090551df634122386e26ab6dfab118
Pablo <pettinarip@gmail.com>
2025-05-05 18:25:13 +0200

    refactor: extract availability and block fee metrics into utility functions

--- app/block/[block]/page.tsx
+++ app/block/[block]/page.tsx
@@ -47,6 +47,8 @@ import { cn } from "@/lib/utils"
 
 import { AVERAGE_LABEL } from "@/lib/constants"
 
+import { getAvailabilityMetrics, getBlockFeeMetrics } from "./utils"
+
 import { db } from "@/db"
 import { fetchBlock } from "@/lib/api/blocks"
 import { timestampToEpoch, timestampToSlot } from "@/lib/beaconchain"
@@ -135,179 +137,11 @@ export default async function BlockDetailsPage({
     totalTTPStats: getTotalTTPStats(multiMachineProofs, timestamp),
   }
 
-  const availabilityMetrics = (stats: typeof singleMachineStats) => [
-    {
-      key: "status-of-proofs",
-      label: "Status of proofs",
-      description: <ProofStatusInfo />,
-      value: <ProofStatus statusCount={proofsPerStatusCount} />,
-    },
-    {
-      key: "fastest-proving-time",
-      label: (
-        <>
-          Fastest <metrics.provingTime.Label />
-        </>
-      ),
-      // TODO: Include team information with fastest proving time
-      description: (
-        <>
-          <TooltipContentHeader>
-            fastest <metrics.provingTime.Label />
-          </TooltipContentHeader>
-          <metrics.provingTime.Details />
-          <Info.Description>
-            Fastest reported proving time for any of the proofs submitted for
-            this block
-          </Info.Description>
-        </>
-      ),
-      value: stats.provingTimeStats?.bestFormatted ?? <Null />,
-    },
-    {
-      key: "avg-proving-time",
-      label: (
-        <>
-          {AVERAGE_LABEL} <metrics.provingTime.Label />
-        </>
-      ),
-      description: (
-        <>
-          <TooltipContentHeader>
-            average <metrics.provingTime.Label />
-          </TooltipContentHeader>
-          <metrics.provingTime.Details average />
-        </>
-      ),
-      value: stats.provingTimeStats?.avgFormatted ?? <Null />,
-    },
-    {
-      key: "fastest-total-ttp",
-      label: (
-        <>
-          Fastest <metrics.totalTTP.Label />
-        </>
-      ),
-      // TODO: Include team information with fastest proving time
-      description: (
-        <>
-          <TooltipContentHeader>
-            fastest <metrics.totalTTP.Label />
-          </TooltipContentHeader>
-          <metrics.totalTTP.Details />
-          <Info.Description>
-            Total time delay between execution block timestamp and completion
-            and publishing of proof
-          </Info.Description>
-          <Info.Description>
-            Fastest time to proof for any of the proofs submitted for this block
-          </Info.Description>
-        </>
-      ),
-      value: stats.totalTTPStats?.bestFormatted ?? <Null />,
-    },
-    {
-      key: "avg-total-ttp",
-      label: (
-        <>
-          {AVERAGE_LABEL} <metrics.totalTTP.Label />
-        </>
-      ),
-      description: (
-        <>
-          <TooltipContentHeader>
-            average <metrics.totalTTP.Label />
-          </TooltipContentHeader>
-          <metrics.totalTTP.Details average />
-          <Info.Description>
-            Total time delay between execution block timestamp and completion
-            and publishing of proof
-          </Info.Description>
-          <Info.Description>
-            Average time to proof for all of the proofs submitted for this block
-          </Info.Description>
-        </>
-      ),
-      value: stats.totalTTPStats?.avgFormatted ?? <Null />,
-    },
-  ]
+  const singleMachineMetrics = getAvailabilityMetrics(singleMachineStats)
+  const multiMachineMetrics = getAvailabilityMetrics(multiMachineStats)
 
-  const blockFeeMetrics = (stats: typeof singleMachineStats) => [
-    {
-      key: "cheapest-cost-per-proof",
-      label: (
-        <>
-          cheapest <metrics.costPerProof.Label />
-        </>
-      ),
-      description: (
-        <>
-          <TooltipContentHeader>
-            cheapest <metrics.costPerProof.Label />
-          </TooltipContentHeader>
-          <metrics.costPerProof.Details />
-        </>
-      ),
-      value: stats.costPerProofStats?.bestFormatted ?? <Null />,
-    },
-    {
-      key: "avg-cost-per-proof",
-      label: (
-        <>
-          {AVERAGE_LABEL} <metrics.costPerProof.Label />
-        </>
-      ),
-      description: (
-        <>
-          <TooltipContentHeader>
-            {AVERAGE_LABEL} <metrics.costPerProof.Label />
-          </TooltipContentHeader>
-          <metrics.costPerProof.Details average />
-        </>
-      ),
-      value: stats.costPerProofStats?.avgFormatted ?? <Null />,
-    },
-    {
-      key: "cheapest-cost-per-mgas",
-      label: (
-        <div>
-          cheapest <metrics.costPerMgas.Label />
-        </div>
-      ),
-      description: (
-        <>
-          <TooltipContentHeader>
-            cheapest <metrics.costPerMgas.Label />
-          </TooltipContentHeader>
-          <metrics.costPerMgas.Details />
-        </>
-      ),
-      value: stats.costPerMgasStats?.bestFormatted ?? <Null />,
-    },
-    {
-      key: "avg-cost-per-mgas",
-      label: (
-        <div>
-          {AVERAGE_LABEL} <metrics.costPerMgas.Label />
-        </div>
-      ),
-      description: (
-        <>
-          <TooltipContentHeader>
-            {AVERAGE_LABEL} <metrics.costPerMgas.Label />
-          </TooltipContentHeader>
-          <metrics.costPerMgas.Details />
-        </>
-      ),
-      value: stats.costPerMgasStats?.avgFormatted ?? <Null />,
-    },
-  ]
-
-  const singleMachineMetrics = availabilityMetrics(singleMachineStats)
-  const multiMachineMetrics = availabilityMetrics(multiMachineStats)
-
-  const singleMachineBlockFeeMetrics = blockFeeMetrics(singleMachineStats)
-  const multiMachineBlockFeeMetrics = blockFeeMetrics(multiMachineStats)
+  const singleMachineBlockFeeMetrics = getBlockFeeMetrics(singleMachineStats)
+  const multiMachineBlockFeeMetrics = getBlockFeeMetrics(multiMachineStats)
 
   return (
     <div className="-mt-40 space-y-20 px-6 md:px-8">
