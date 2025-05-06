6aff9609bc6f7d8ddb63ed9eb35934b1b3e6c638
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-05-05 21:52:50 -0700

    feat: clean up not-found pages for consistency

--- app/zkvms/[slug]/not-found.tsx
+++ app/zkvms/[slug]/not-found.tsx
@@ -13,9 +13,12 @@ export default function NotFound() {
     <div className="flex flex-col items-center gap-4 text-center">
       <LightningBolt className="size-40 stroke-[0.5px] text-body-secondary" />
       <h1 className="mb-4 font-mono text-3xl text-primary">
-        No zkVM found with name {slug}
+        No zkVM found with name {decodeURIComponent(slug)}
       </h1>
       <Divider className="my-6" />
+      <ButtonLink href="/zkvms" size="lg">
+        Explore all zkVMs
+      </ButtonLink>
       <ButtonLink href="/" variant="outline" size="lg">
         Back to homepage
       </ButtonLink>
