1e5235ee92d30808e6f9fb5603fe9471920a38b1
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-05-05 18:17:22 -0700

    feat: add zkvm not-found page

---
+++ app/zkvms/[slug]/not-found.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { useParams } from "next/navigation"
+
+import LightningBolt from "@/components/svgs/lightning-bolt.svg"
+import { ButtonLink } from "@/components/ui/button"
+import { Divider } from "@/components/ui/divider"
+
+export default function NotFound() {
+  const slug = useParams().slug as string
+
+  return (
+    <div className="flex flex-col items-center gap-4 text-center">
+      <LightningBolt className="size-40 stroke-[0.5px] text-body-secondary" />
+      <h1 className="mb-4 font-mono text-3xl text-primary">
+        No zkVM found with name {slug}
+      </h1>
+      <Divider className="my-6" />
+      <ButtonLink href="/" variant="outline" size="lg">
+        Back to homepage
+      </ButtonLink>
+    </div>
+  )
+}
