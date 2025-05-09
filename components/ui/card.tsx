06083cc7868395268c13ef5df09abef198eae17e
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-05-08 19:10:56 -0700

    feat: add borderGradient variant to Card

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,20 +1,53 @@
 import * as React from "react"
+import { cva, VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
+const cardVariants = cva("rounded-4xl p-6", {
+  variants: {
+    variant: {
+      default: cn(
+        "border bg-white/10 bg-gradient-to-b from-body/[0.06] to-body/[0.03] dark:bg-black/10",
+        "space-y-6 md:space-y-12 md:px-12 md:py-8"
+      ),
+      borderGradient: "relative bg-background",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+  },
+})
+
 const Card = React.forwardRef<
   HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "space-y-6 rounded-4xl border bg-white/10 bg-gradient-to-b from-body/[0.06] to-body/[0.03] p-6 dark:bg-black/10 md:space-y-12 md:px-12 md:py-8",
-      className
-    )}
-    {...props}
-  />
-))
+  React.HTMLAttributes<HTMLDivElement> &
+    VariantProps<typeof cardVariants> & {
+      gradientClasses?: string
+    }
+>(({ className, variant, gradientClasses, ...props }, ref) =>
+  variant === "borderGradient" ? (
+    <div
+      ref={ref}
+      className={cn(cardVariants({ variant }), className)}
+      {...props}
+    >
+      <div
+        className={cn(
+          "absolute -inset-px z-[-2] rounded-[calc(2rem_+_1px)] bg-gradient-to-tl from-primary to-primary/10",
+          gradientClasses
+        )}
+      />
+      <div className="absolute -inset-px z-[-1] rounded-4xl bg-black/10" />
+      <div className="space-y-6 md:space-y-12">{props.children}</div>
+    </div>
+  ) : (
+    <div
+      ref={ref}
+      className={cn(cardVariants({ variant }), className)}
+      {...props}
+    />
+  )
+)
 Card.displayName = "Card"
 
 const CardHeader = React.forwardRef<
