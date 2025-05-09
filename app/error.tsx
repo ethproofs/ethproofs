51a6b5e0b127bcc63649146d9aa1a90181e92985
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-05-08 22:46:46 -0700

    feat: update error page to match theme

--- app/error.tsx
+++ app/error.tsx
@@ -1,15 +1,24 @@
 "use client" // Error boundaries must be Client Components
 
 import ProofCircle from "@/components/svgs/proof-circle.svg"
-import { HeroSection } from "@/components/ui/hero"
+import { Button, ButtonLink } from "@/components/ui/button"
+import { Divider } from "@/components/ui/divider"
 
 export default function Error() {
   return (
-    <HeroSection className="space-y-4">
-      <h1 className="flex flex-col items-center gap-4 font-mono md:flex-row">
-        500 <ProofCircle className="inline text-primary" />
-      </h1>
-      <p className="text-center md:text-start">Internal server error</p>
-    </HeroSection>
+    <div className="flex flex-col items-center gap-4 text-center">
+      <ProofCircle className="size-40 -translate-x-4 stroke-[1.25px] text-primary" />
+      <h1 className="mb-4 font-mono font-normal text-primary">500</h1>
+      <p className="text-lg">Internal server error</p>
+      <Divider className="my-6" />
+      <div className="flex flex-col gap-6">
+        <Button onClick={() => window.location.reload()} size="lg">
+          Refresh page
+        </Button>
+        <ButtonLink href="/" variant="outline" size="lg">
+          Back to homepage
+        </ButtonLink>
+      </div>
+    </div>
   )
 }
