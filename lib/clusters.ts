55f44672bc09e2030748835291479e7ecc42ba27
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-05-09 15:15:24 -0700

    feat: add isMultiMachineCluster util

--- lib/clusters.ts
+++ lib/clusters.ts
@@ -1,5 +1,14 @@
-import { ClusterMachineBase } from "./types"
+import type { ClusterMachineBase, MachineBase } from "./types"
 
 export const hasPhysicalMachines = (clusterMachines: ClusterMachineBase[]) => {
   return clusterMachines.some((cm) => cm.machine_id !== null)
+}
+
+export const isMultiMachineCluster = (
+  clusterMachines: ClusterMachineBase[]
+) => {
+  return (
+    clusterMachines.length > 1 ||
+    clusterMachines.some((config) => config.machine_count > 1)
+  )
 }
