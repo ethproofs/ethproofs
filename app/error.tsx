bc472dec18ee630619e5f898fa9305da81c19abd
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-05-09 19:48:53 -0700

    feat: refreshing icon spinner for 500

--- app/error.tsx
+++ app/error.tsx
@@ -1,10 +1,20 @@
 "use client" // Error boundaries must be Client Components
 
+import { useState } from "react"
+
 import ProofCircle from "@/components/svgs/proof-circle.svg"
+import RefreshCW from "@/components/svgs/refresh-cw.svg"
 import { Button, ButtonLink } from "@/components/ui/button"
 import { Divider } from "@/components/ui/divider"
 
+import { cn } from "@/lib/utils"
+
 export default function Error() {
+  const [refreshing, setRefreshing] = useState(false)
+  const handleRefresh = () => {
+    setRefreshing(true)
+    window.location.reload()
+  }
   return (
     <div className="flex flex-col items-center gap-4 text-center">
       <ProofCircle className="size-40 -translate-x-4 stroke-[1.25px] text-primary" />
@@ -12,7 +22,11 @@ export default function Error() {
       <p className="text-lg">Internal server error</p>
       <Divider className="my-6" />
       <div className="flex flex-col gap-6">
-        <Button onClick={() => window.location.reload()} size="lg">
+        <Button
+          onClick={handleRefresh}
+          size="lg"
+        >
+          <RefreshCW className={cn(refreshing && "animate-spin")} />
           Refresh page
         </Button>
         <ButtonLink href="/" variant="outline" size="lg">
