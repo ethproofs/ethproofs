00d52ecbfa49dbac5e8ae142eaa928691ab475c1
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-05-13 15:10:35 -0700

    refactor: rearrange legend to wrap under options
    update card footer base padding

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -63,7 +63,7 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 py-0", className)} {...props} />
+  <div ref={ref} className={className} {...props} />
 ))
 CardContent.displayName = "CardContent"
 
@@ -73,7 +73,7 @@ const CardFooter = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
+    className={cn("flex items-center py-4", className)}
     {...props}
   />
 ))
