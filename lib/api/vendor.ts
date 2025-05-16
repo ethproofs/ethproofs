18538d5bd1f74e98a82f09ec0abed3114bd366ea
Pablo <pettinarip@gmail.com>
2025-05-16 15:33:59 +0200

    handle only vendor cases

--- lib/api/vendor.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "@/db"
-
-export const getVendorByUserId = async (userId: string) => {
-  const vendor = await db.query.vendors.findFirst({
-    where: (vendors, { eq }) => eq(vendors.user_id, userId),
-  })
-  return vendor
-}
