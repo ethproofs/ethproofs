5f8f8b05c061119389973409df4f6c0a7e2d8219
fbwoolf <fara@trustmachines.co>
2025-07-08 20:08:39 -0400

    refactor: zkvms active tab ui

--- components/SoftwareAccordion/InactiveSoftwareAccordion.tsx
+++ components/SoftwareAccordion/InactiveSoftwareAccordion.tsx
@@ -1,14 +1,13 @@
-import { Vendor, Zkvm, ZkvmMetrics, ZkvmVersion } from "@/lib/types"
-
-import { Accordion } from "../ui/accordion"
-import { MetricBox, MetricInfo, MetricLabel } from "../ui/metric"
-import { TooltipContentHeader } from "../ui/tooltip"
+import { Vendor, Zkvm, ZkvmVersion } from "@/lib/types"
 
 import { InactiveSoftwareAccordionItem } from "./InactiveSoftwareAccordionItem"
+import SoftwareAccordionLayout from "./SoftwareAccordion.layout"
 
 export type InactiveZkvm = Zkvm & {
   versions: ZkvmVersion[]
   vendor: Vendor
+  totalClusters: number
+  activeClusters: number
 }
 
 interface InactiveSoftwareAccordionProps {
@@ -19,53 +18,7 @@ export const InactiveSoftwareAccordion = ({
   zkvms,
 }: InactiveSoftwareAccordionProps) => {
   return (
-    <Accordion
-      type="multiple"
-      className="grid w-full grid-cols-[1fr_repeat(7,_auto)]"
-    >
-      <div className="col-span-8 grid grid-cols-subgrid gap-4 text-center">
-        <MetricBox className="col-start-2">
-          <MetricLabel>open source</MetricLabel>
-        </MetricBox>
-        <MetricBox className="col-start-3">
-          <MetricLabel>dual license</MetricLabel>
-        </MetricBox>
-        <MetricBox className="col-start-4">
-          <MetricLabel className="normal-case">proving L1</MetricLabel>
-        </MetricBox>
-        <MetricBox className="col-start-5">
-          <MetricLabel>
-            <MetricInfo label="latest version">
-              <TooltipContentHeader>latest version</TooltipContentHeader>
-              The most recent version of this zkVM being used by teams on
-              ethproofs.org
-            </MetricInfo>
-          </MetricLabel>
-        </MetricBox>
-        <MetricBox className="col-start-6">
-          <MetricLabel>
-            <MetricInfo label="ISA">
-              <TooltipContentHeader>
-                instruction set architecture
-              </TooltipContentHeader>
-              Defines the instruction set this zkVM implements to generate
-              zero-knowledge proofs for Ethereum transactions. The ISA
-              determines which EVM operations can be efficiently proven and
-              verified on-chain
-            </MetricInfo>
-          </MetricLabel>
-        </MetricBox>
-        <MetricBox className="col-start-7">
-          <MetricLabel>
-            <MetricInfo label="used by">
-              <TooltipContentHeader>
-                number of active clusters
-              </TooltipContentHeader>
-              Number of active clusters that use this zkVM on ethproofs.org
-            </MetricInfo>
-          </MetricLabel>
-        </MetricBox>
-      </div>
+    <SoftwareAccordionLayout>
       {zkvms.map((zkvm) => (
         <InactiveSoftwareAccordionItem
           key={zkvm.id}
@@ -73,7 +26,7 @@ export const InactiveSoftwareAccordion = ({
           zkvm={zkvm}
         />
       ))}
-    </Accordion>
+    </SoftwareAccordionLayout>
   )
 }
 
--- components/SoftwareAccordion/InactiveSoftwareAccordionItem.tsx
+++ components/SoftwareAccordion/InactiveSoftwareAccordionItem.tsx
@@ -1,15 +1,12 @@
-import { Check, X as RedX } from "lucide-react"
 import { type AccordionItemProps } from "@radix-ui/react-accordion"
 
-import type { Slices, ZkvmMetrics } from "@/lib/types"
+import type { Slices } from "@/lib/types"
 
-import { DisplayTeamLink } from "../DisplayTeamLink"
 import Pizza from "../Pizza"
-import { AccordionItem, AccordionTrigger } from "../ui/accordion"
-import Link from "../ui/link"
-import { Progress } from "../ui/progress"
+import { AccordionTrigger } from "../ui/accordion"
 
 import { InactiveZkvm } from "./InactiveSoftwareAccordion"
+import SoftwareAccordionItemLayout from "./SoftwareAccordionItem.layout"
 
 const disabledSlices: Slices = [
   { level: undefined },
@@ -29,67 +26,17 @@ export const InactiveSoftwareAccordionItem = ({
   zkvm: InactiveZkvm
 }) => {
   return (
-    <AccordionItem value={value} className="col-span-8 grid grid-cols-subgrid">
-      <div className="col-span-8 grid grid-cols-subgrid items-center justify-items-center gap-12 text-nowrap border-b p-px px-6">
-        <div className="col-start-1 flex items-center gap-3 justify-self-start">
-          <Link
-            href={`/zkvms/${zkvm.slug}`}
-            className="block text-2xl text-primary hover:text-primary-light hover:underline"
-          >
-            {zkvm.name}
-          </Link>
-          <span className="block font-mono text-sm italic text-body-secondary">
-            by
-          </span>
-          <div className="min-w-24">
-            <DisplayTeamLink team={zkvm.vendor} className="block" />
-          </div>
-        </div>
-        <div id="is-open-source" className="col-start-2">
-          {zkvm.is_open_source ? (
-            <Check className="text-level-best" />
-          ) : (
-            <RedX className="text-level-worst" />
-          )}
-        </div>
-        <div id="dual-licenses" className="col-start-3">
-          {zkvm.dual_licenses ? (
-            <Check className="text-level-best" />
-          ) : (
-            <RedX className="text-level-worst" />
-          )}
-        </div>
-        <div id="is-proving-mainnet" className="col-start-4">
-          {zkvm.is_proving_mainnet ? (
-            <Check className="text-level-best" />
-          ) : (
-            <RedX className="text-level-worst" />
-          )}
-        </div>
-        <div id="version" className="col-start-5">
-          {zkvm.versions[0].version}
-        </div>
-        <div id="isa" className="col-start-6 min-w-14 text-center">
-          {zkvm.isa}
-        </div>
-        <div id="used-by" className="relative col-start-7 min-w-16">
-          <div className="w-full text-center">
-            0/
-            <span className="text-xs">0</span>
-          </div>
-          <Progress
-            value={0}
-            className="absolute -bottom-1 left-0 h-[2px] w-full"
-          />
-        </div>
-
+    <SoftwareAccordionItemLayout
+      value={value}
+      zkvm={zkvm}
+      trigger={
         <AccordionTrigger
           disabled
           className="col-start-8 my-4 h-fit gap-2 rounded-full border-2 bg-background-highlight p-0.5 pe-2 text-4xl text-border [&>svg]:size-6"
         >
           <Pizza slices={disabledSlices} disableEffects />
         </AccordionTrigger>
-      </div>
-    </AccordionItem>
+      }
+    />
   )
 }
