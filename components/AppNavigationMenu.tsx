8b15a4fba804b2981a15cde5f9ae1135d9e41f2e
fbwoolf <6493321+fbwoolf@users.noreply.github.com>
2025-08-26 15:23:50 -0400

    refactor: sidebar navigation

--- components/AppNavigationMenu.tsx
+++ components/AppNavigationMenu.tsx
@@ -13,7 +13,7 @@ import {
   Zap,
 } from "lucide-react"
 
-import EthProofsLogo from "@/components/svgs/eth-proofs-logo.svg"
+import EthproofsIcon from "@/components/svgs/ethproofs-icon.svg"
 import GitHub from "@/components/svgs/github.svg"
 import Twitter from "@/components/svgs/twitter.svg"
 import Link from "@/components/ui/link"
@@ -23,56 +23,48 @@ import { SITE_REPO } from "@/lib/constants"
 
 import SearchInput from "./header/SearchInput"
 import ThemeSwitch from "./header/ThemeSwitch"
-import { DrawerClose } from "./ui/drawer"
 import {
+  Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
   SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
+  SidebarMenuButton,
   SidebarMenuItem,
+  SidebarRail,
 } from "./ui/sidebar"
 import { Skeleton } from "./ui/skeleton"
 
-type AppNavigationMenuProps = {
-  insideDrawer?: boolean
-}
-
-const AppNavigationMenu = ({ insideDrawer }: AppNavigationMenuProps) => {
-  const MenuItem = (props: { children: React.ReactNode }) =>
-    insideDrawer ? (
-      <SidebarMenuItem>
-        <DrawerClose asChild {...props} />
-      </SidebarMenuItem>
-    ) : (
-      <SidebarMenuItem {...props} />
-    )
-
+const AppNavigationMenu = () => {
   const verticalFade = {
     mask: `linear-gradient(to bottom, transparent 0rem, white 2rem, white calc(100% - 2rem), transparent 100%)`,
   }
 
-  const menuItemLinkClass =
+  const customSidebarMenuItemLinkClass =
     "inline-flex items-center text-body hover:text-primary [&>svg:first-child]:me-2 [&>svg:first-child]:size-4"
 
   return (
-    <>
+    <Sidebar
+      collapsible="icon"
+      className="bg-gradient-to-b from-sidebar to-background font-body"
+    >
       <SidebarHeader className="mt-11 space-y-10">
-        <div className="flex items-end justify-between gap-4">
-          <Link href="/">
-            <EthProofsLogo className="text-[2.625rem] text-body" />
+        <div className="flex items-end gap-2">
+          <EthproofsIcon />
+          <Link className="text-2xl font-bold italic text-body" href="/">
+            Ethproofs
           </Link>
 
-          <Suspense fallback={<Skeleton className="h-8 w-12 rounded-full" />}>
+          {/* <Suspense fallback={<Skeleton className="h-8 w-12 rounded-full" />}>
             <ThemeSwitch />
-          </Suspense>
+          </Suspense> */}
         </div>
-        <SearchInput
+        {/* <SearchInput
           aria-label="search by block"
           placeholder="search by block"
-          insideDrawer={insideDrawer}
-        />
+        /> */}
       </SidebarHeader>
 
       <SidebarContent style={verticalFade}>
@@ -82,97 +74,97 @@ const AppNavigationMenu = ({ insideDrawer }: AppNavigationMenuProps) => {
         >
           <SidebarGroup>
             <SidebarGroupLabel>home</SidebarGroupLabel>
-            <MenuItem>
-              <Link href="/" className={menuItemLinkClass}>
+            <SidebarMenuItem>
+              <Link className={customSidebarMenuItemLinkClass} href="/">
                 <TrendingUp />
-                dashboard
+                <span>dashboard</span>
               </Link>
-            </MenuItem>
+            </SidebarMenuItem>
           </SidebarGroup>
           <SidebarGroup>
             <SidebarGroupLabel>explore</SidebarGroupLabel>
-            <MenuItem>
-              <Link href="/zkvms" className={menuItemLinkClass}>
+            <SidebarMenuItem>
+              <Link className={customSidebarMenuItemLinkClass} href="/zkvms">
                 <Zap />
-                zkVMs
+                <span>zkVMs</span>
               </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="/clusters" className={menuItemLinkClass}>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <Link className={customSidebarMenuItemLinkClass} href="/clusters">
                 <Cpu />
-                provers
+                <span>provers</span>
               </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="/blocks" className={menuItemLinkClass}>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <Link className={customSidebarMenuItemLinkClass} href="/blocks">
                 <Box />
-                blocks
+                <span>blocks</span>
               </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="/killers" className={menuItemLinkClass}>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <Link className={customSidebarMenuItemLinkClass} href="/killers">
                 <Bomb />
-                killers
+                <span>killers</span>
               </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="/teams" className={menuItemLinkClass}>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <Link className={customSidebarMenuItemLinkClass} href="/teams">
                 <Users />
-                teams
+                <span>teams</span>
               </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="/status" className={menuItemLinkClass}>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <Link className={customSidebarMenuItemLinkClass} href="/status">
                 <HeartPulse />
-                status
+                <span>status</span>
               </Link>
-            </MenuItem>
+            </SidebarMenuItem>
           </SidebarGroup>
           <SidebarGroup>
             <SidebarGroupLabel>learn</SidebarGroupLabel>
-            <MenuItem>
-              <Link href="/learn" className={menuItemLinkClass}>
+            <SidebarMenuItem>
+              <Link className={customSidebarMenuItemLinkClass} href="/learn">
                 <Library />
-                learn
+                <span>learn</span>
               </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="/api.html" className={menuItemLinkClass}>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <Link className={customSidebarMenuItemLinkClass} href="/api.html">
                 <Target />
-                API
+                <span>API</span>
               </Link>
-            </MenuItem>
+            </SidebarMenuItem>
           </SidebarGroup>
           <SidebarGroup>
             <SidebarGroupLabel>contribute</SidebarGroupLabel>
-            <MenuItem>
+            <SidebarMenuItem>
               <Link
+                className={customSidebarMenuItemLinkClass}
                 href={URL_GITHUB_REPO}
-                className={menuItemLinkClass}
                 hideArrow
               >
                 <GitHub />
-                build Ethproofs
+                <span>build Ethproofs</span>
               </Link>
-            </MenuItem>
-            <MenuItem>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
               <Link
+                className={customSidebarMenuItemLinkClass}
                 href={new URL(
                   SITE_REPO + "/issues/new/choose/",
                   "https://github.com"
                 ).toString()}
-                className={menuItemLinkClass}
                 hideArrow
               >
                 <Bug />
-                spot a bug?
+                <span>spot a bug?</span>
               </Link>
-            </MenuItem>
+            </SidebarMenuItem>
           </SidebarGroup>
         </SidebarMenu>
       </SidebarContent>
 
-      <SidebarFooter>
+      {/* <SidebarFooter>
         <div className="flex items-center gap-x-6 pb-2 font-sans font-bold [&>a]:text-2xl [&>a]:text-body">
           <Link href={URL_GITHUB_REPO} hideArrow>
             <GitHub />
@@ -197,8 +189,9 @@ const AppNavigationMenu = ({ insideDrawer }: AppNavigationMenuProps) => {
           team
         </div>
         <div className="text-primary">public goods are good</div>
-      </SidebarFooter>
-    </>
+      </SidebarFooter> */}
+      {/* <SidebarRail /> */}
+    </Sidebar>
   )
 }
 
