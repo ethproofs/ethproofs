b2def986902a8af3f53ccbc5a77bc87e2d676b5a
fbwoolf <6493321+fbwoolf@users.noreply.github.com>
2025-09-08 19:39:00 -0400

    refactor: design system colors

--- app/providers.tsx
+++ app/providers.tsx
@@ -44,7 +44,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class">
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <SidebarProvider>{children}</SidebarProvider>
       </ThemeProvider>
     </QueryClientProvider>
