d6ec9beb585140b2af282c7d35d83d5e901a4cab
fbwoolf <6493321+fbwoolf@users.noreply.github.com>
2025-10-15 11:02:38 -0400

    fix: add where clause to db updates

--- app/api/v0/proofs/proving/route.ts
+++ app/api/v0/proofs/proving/route.ts
@@ -1,3 +1,4 @@
+import { ne } from "drizzle-orm"
 import { revalidateTag } from "next/cache"
 import { ZodError } from "zod"
 
@@ -114,6 +115,7 @@ export const POST = withAuth(async ({ request, user, timestamp }) => {
           proof_status: "proving",
           proving_timestamp: timestamp,
         },
+        where: ne(proofs.proof_status, "proved"),
       })
       .returning({ proof_id: proofs.proof_id })
 
