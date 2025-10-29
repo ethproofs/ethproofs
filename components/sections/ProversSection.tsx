bf3e5e22e35f7b1104f54e48514b956d7cf11bd6
fbwoolf <6493321+fbwoolf@users.noreply.github.com>
2025-10-29 12:37:05 -0400

    fix: sorting on dashboard

--- components/sections/ProversSection.tsx
+++ components/sections/ProversSection.tsx
@@ -42,13 +42,13 @@ const ProversSection = async () => {
     }
   })
 
-  const singleMachineClusters = clusters.filter(
-    (cluster) => !cluster.is_multi_machine
-  )
+  const singleMachineClusters = clusters
+    .filter((cluster) => !cluster.is_multi_machine)
+    .sort((a, b) => a.avg_time - b.avg_time)
 
-  const multiMachineClusters = clusters.filter(
-    (cluster) => cluster.is_multi_machine
-  )
+  const multiMachineClusters = clusters
+    .filter((cluster) => cluster.is_multi_machine)
+    .sort((a, b) => a.avg_time - b.avg_time)
 
   return (
     <Card className="!p-0 !pb-6 md:!pb-8">
